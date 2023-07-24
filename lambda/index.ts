a74bac20a757ac786dca9d958ad1b2b2f34a0151
Daniel Van Der Ploeg <daniel.vanderploeg@aligent.com.au>
2023-07-24 12:55:37 +0930

    feat MICRO-89: add basic lambda structure

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,9 +1,49 @@
 import { createBuiltMeshHTTPHandler } from '../src/meshrc/.mesh';
-import express from 'express';
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import type { Handler } from '@aws-cdk/aws-lambda';
 
-const app = express();
-app.use('/graphql', createBuiltMeshHTTPHandler());
+interface ServerContext {
+    event: APIGatewayEvent;
+    lambdaContext: Context;
+}
 
-app.listen(4000, () => {
-    console.log('Mesh running');
-});
+const meshHTTP = createBuiltMeshHTTPHandler<ServerContext>();
+
+export async function handler(
+    event: APIGatewayEvent,
+    lambdaContext: Context
+): Promise<APIGatewayProxyResult> {
+    const url = new URL(event.path, 'http://localhost');
+    if (event.queryStringParameters != null) {
+        for (const name in event.queryStringParameters) {
+            const value = event.queryStringParameters[name];
+            if (value != null) {
+                url.searchParams.set(name, value);
+            }
+        }
+    }
+
+    const response = await meshHTTP.fetch(
+        url,
+        {
+            method: event.requestContext.httpMethod,
+            headers: event.headers as HeadersInit,
+            body: event.body
+                ? Buffer.from(event.body, event.isBase64Encoded ? 'base64' : 'utf8')
+                : undefined,
+        },
+        {
+            event,
+            lambdaContext,
+        }
+    );
+
+    const responseHeaders: Record<string, string> = Object.fromEntries(response.headers.entries());
+
+    return {
+        statusCode: response.status,
+        headers: responseHeaders,
+        body: await response.text(),
+        isBase64Encoded: false,
+    };
+}
