0f70708610b4c22f873eec74f9d86f3f8c1e2c14
jack.mcloughlin <jackmaca@msn.com>
2023-07-25 11:43:42 +0930

    MICRO-90 added resolver for generateCustomerToken and got token from BC

--- src/bigcommerce/resolvers/mutations/generate-customer-token.ts
+++ src/bigcommerce/resolvers/mutations/generate-customer-token.ts
@@ -1,7 +1,20 @@
-import { mockExportCustomerToken } from '../mocks/generate-customer-token';
+import { getBcGraphqlToken } from '../lib/bc-calls';
 
 export const generateCustomerTokenResolver = {
-    resolve: () => {
-        return mockExportCustomerToken;
+    resolve: async () => {
+        const dateCreated = Math.round(new Date().getTime() / 1000);
+        const dateEndAt = dateCreated + 86400; // Adding 1 day
+
+        const data = {
+            allowed_cors_origins: [],
+            channel_id: 1,
+            expires_at: dateEndAt,
+        };
+
+        const bcGraphqlToken = await getBcGraphqlToken(data);
+
+        return {
+            token: bcGraphqlToken,
+        };
     },
 };
---
+++ src/bigcommerce/types/index.ts
@@ -0,0 +1,5 @@
+export interface BcGraphqlTokenData {
+    allowed_cors_origins: [] | string[];
+    channel_id: number;
+    expires_at: number;
+}
