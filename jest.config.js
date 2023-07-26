a3a056e019c2230b449a3131727a8195deda5658
jack.mcloughlin <jackmaca@msn.com>
2023-07-26 11:20:13 +0930

    MICRO-96 added jest package and sample tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-    roots: ["<rootDir>/test"],
-    testMatch: ["**/*.test.ts"],
+    testEnvironment: 'node',
     transform: {
-        "^.+\\.tsx?$": "ts-jest",
+        '^.+\\.m?[tj]sx?$': [
+            'ts-jest',
+            {
+                isolatedModules: true,
+            },
+        ],
+    },
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
     },
 };
