bcb711170ef5efba4f82cf60d7e6ee8a3f5adb49
Brett Cutting <brett.cutting@aligent.com.au>
2023-07-27 14:23:04 +1000

    MICRO-90: add a encode-decode util file to add related functions Add a btoa function to replicate window.btoa

---
+++ src/utils/encode-decode.ts
@@ -0,0 +1,16 @@
+import { Buffer } from 'buffer';
+
+/**
+ * Encodes a string to base64.
+ *
+ * mostly used to encodes id's to uids.
+ * e.g. btoa("6") = "Ng=="
+ *
+ * The counterpart of btoa is atob
+ * atob("Ng==") = "6"
+ *
+ * @param input
+ */
+export const btoa = (input: string): string => {
+    return Buffer.from(input).toString('base64');
+};
