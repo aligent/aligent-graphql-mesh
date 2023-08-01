5b45e93c4d66d4a0d9f9083c00d98de59ae7e8dd
Brett Cutting <brett.cutting@aligent.com.au>
2023-08-02 08:05:58 +1000

    MICRO-90: add underscores to indicate params not used

--- src/bigcommerce/resolvers/queries/cart.ts
+++ src/bigcommerce/resolvers/queries/cart.ts
@@ -3,7 +3,7 @@ import { getCart } from '../requests/bc-graphql-calls';
 import { getTransformedCartData } from '../../factories/transform-cart-data';
 
 export const cartResolver: QueryResolvers['cart'] = {
-    resolve: async (root, args, context, info): Promise<any> => {
+    resolve: async (_root, args, _context, _info): Promise<any> => {
         const response = await getCart(args.cart_id);
         if (!response?.entityId) return null;
         return getTransformedCartData(response);
