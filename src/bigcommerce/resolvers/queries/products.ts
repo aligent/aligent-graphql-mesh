83e664ef8b7597b2c66854ff12da804fc9c89723
jack.mcloughlin <jackmaca@msn.com>
2023-08-01 10:10:21 +0930

    MICRO-96 added type to resolver

--- src/bigcommerce/resolvers/queries/products.ts
+++ src/bigcommerce/resolvers/queries/products.ts
@@ -1,11 +1,13 @@
-import { Products, ProductsResolvers } from '../../../meshrc/.mesh';
+import { QueryResolvers } from '../../../meshrc/.mesh';
 import { createAcReadyProduct } from '../../factories/transform-products.data';
 import { getBcProductGraphql } from '../requests/bc-graphql-calls';
 
-export const productsResolver: ProductsResolvers<Products> = {
+export const productsResolver: QueryResolvers['products'] = {
     resolve: async (_root, args, _context, _info) => {
-        const bcProduct = await getBcProductGraphql(args.filter.sku.eq);
+        const sku = args.filter?.sku?.eq || "";
 
-        return  createAcReadyProduct(bcProduct);
+        const bcProduct = await getBcProductGraphql(sku);
+
+        return createAcReadyProduct(bcProduct);
     },
 };
