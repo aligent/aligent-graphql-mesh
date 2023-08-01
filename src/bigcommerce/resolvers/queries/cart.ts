dacee8c5dea791560c4a964383e2656ae9aee94b
Brett Cutting <brett.cutting@aligent.com.au>
2023-08-01 11:32:35 +1000

    MICRO-90: add QueryResolvers['cart'] to cartResolver

--- src/bigcommerce/resolvers/queries/cart.ts
+++ src/bigcommerce/resolvers/queries/cart.ts
@@ -1,15 +1,9 @@
+import { QueryResolvers } from '../../../meshrc/.mesh';
 import { getCart } from '../requests/bc-graphql-calls';
 import { getTransformedCartData } from '../../factories/transform-cart-data';
 
-export const cartResolver = {
-    resolve: async (
-        root: any,
-        args: { cart_id: string },
-        context: {
-            headers: { authorization: string };
-        },
-        info: any
-    ): Promise<any> => {
+export const cartResolver: QueryResolvers['cart'] = {
+    resolve: async (root, args, context, info): Promise<any> => {
         const response = await getCart(args.cart_id);
         if (!response?.entityId) return null;
         return getTransformedCartData(response);
