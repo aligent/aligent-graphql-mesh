4aab9b184e393d6e825981a80bc2f48e8018b63a
Brett Cutting <brett.cutting@aligent.com.au>
2023-08-02 14:16:56 +1000

    MICRO-90: add return promise type to the cart resolver

--- src/bigcommerce/resolvers/queries/cart.ts
+++ src/bigcommerce/resolvers/queries/cart.ts
@@ -1,9 +1,9 @@
-import { QueryResolvers } from '../../../meshrc/.mesh';
+import { Cart, Maybe, QueryResolvers } from '../../../meshrc/.mesh';
 import { getCart } from '../requests/bc-graphql-calls';
 import { getTransformedCartData } from '../../factories/transform-cart-data';
 
 export const cartResolver: QueryResolvers['cart'] = {
-    resolve: async (_root, args, _context, _info): Promise<any> => {
+    resolve: async (_root, args, _context, _info): Promise<Maybe<Cart>> => {
         const response = await getCart(args.cart_id);
         if (!response?.entityId) return null;
         return getTransformedCartData(response);
