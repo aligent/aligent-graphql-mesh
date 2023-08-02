fb43feb37b744772fd763ce96a4d851ea6d55d65
jan.plank <jan.plank@aligent.com.au>
2023-08-02 12:03:11 +0930

    MICRO-94: add store config transform (WIP)

--- src/bigcommerce/resolvers/queries/store-config.ts
+++ src/bigcommerce/resolvers/queries/store-config.ts
@@ -15,12 +15,23 @@ export const storeConfigResolver: StoreConfigResolvers<StoreConfig> = {
 async function transformStoreConfig(bcStoreConfig: BcStoreConfigMetafields): Promise<StoreConfig> {
     const metafields = bcStoreConfig.channel.metafields.edges;
 
+    // for (const metafield of metafields) {
+    //     const key = metafield.key;
+    //     const value = metafield.value;
+    //     console.log(key);
+    // }
+    //
+    // metafields.map((node)=> {
+    //     node.key
+    //     node.value
+    // })
+
     //TODO map metafield to magento format
     const storeConfigTransformed: StoreConfig = {
         category_url_suffix: 'bla',
+        grid_per_page: 50,
         contact_enabled: false, newsletter_enabled: false, pwa_base_url: '', returns_enabled: ''
-
-    }
+    };
 
     return storeConfigTransformed;
 }
