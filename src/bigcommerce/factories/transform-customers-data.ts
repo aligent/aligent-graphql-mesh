157b7dd877cf9e53fb6a5bcccbd0bd5dab567f08
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2023-08-03 15:12:41 +0930

    chore: add todo about remapping customer return type

--- src/bigcommerce/factories/transform-customers-data.ts
+++ src/bigcommerce/factories/transform-customers-data.ts
@@ -5,6 +5,8 @@ export const transformCustomerData = (bcCustomer: BcCustomer): Customer => {
     return {
         id: bcCustomer.id,
         // all data below is to satisfy TS, FE only needs id
+        // TODO: Try to reduce typescript object requirements with mappers:
+        // https://the-guild.dev/graphql/codegen/plugins/typescript/typescript-resolvers#mappers
         allow_remote_shopping_assistance: true,
         reviews: {
             items: [],
