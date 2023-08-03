9cb20b01adfef54f76beec1a3683cdc5ffe569bc
Thilina Gunasekara <thilina@aligent.com.au>
2023-08-03 17:53:09 +0930

    MICRO-114: fix formatting

--- src/bigcommerce/resolvers/mutations/reorder-items.ts
+++ src/bigcommerce/resolvers/mutations/reorder-items.ts
@@ -1,6 +1,6 @@
 import { MutationResolvers } from '../../../meshrc/.mesh';
 
-export const reorderItemsResolver: MutationResolvers['reorderItems']= {
+export const reorderItemsResolver: MutationResolvers['reorderItems'] = {
     resolve: (_root, _args, _context, _info) => {
         // TODO
         return null;
