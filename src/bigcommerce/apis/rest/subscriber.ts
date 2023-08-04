a791a285bf3f8dd4d7f124637a2859aea1e83cfb
jan.plank <jan.plank@aligent.com.au>
2023-08-04 13:58:47 +0930

    MICRO-120: add subscribe email resolver

---
+++ src/bigcommerce/apis/rest/subscriber.ts
@@ -0,0 +1,17 @@
+import { BcSubscriber } from '../../types';
+import { bcPost } from './client';
+
+
+export const createSubscriber = async(email: string): Promise<BcSubscriber> => {
+    console.log('In createSubscriber');
+    const path = `/v3/customers/subscribers`;
+
+    const data = {
+        email: email
+    }
+
+    const response = await bcPost(path, data);
+    console.log(JSON.stringify(response.data));
+
+    return response;
+}
--- src/bigcommerce/resolvers/mutations/subscribe-email-to-newsletter.ts
+++ src/bigcommerce/resolvers/mutations/subscribe-email-to-newsletter.ts
@@ -1,8 +1,30 @@
-import { MutationResolvers } from '../../../meshrc/.mesh';
+import { MutationResolvers, SubscribeEmailToNewsletterOutput } from '../../../meshrc/.mesh';
+import { logAndThrowError } from '../../../utils/error-handling';
+import { createSubscriber } from '../../apis/rest/subscriber';
+import { BcSubscriber } from '../../types';
 
 export const subscribeEmailToNewsletterResolver: MutationResolvers['subscribeEmailToNewsletter'] = {
-    resolve: (_root, _args, _context, _info) => {
-        // TODO
-        return null;
+    resolve: async (_root, _args, _context, _info) => {
+        console.log('In subscribeEmailToNewsletterResolver');
+        if (
+            !_args.email
+        ) {
+            return logAndThrowError(
+                new Error('Missing email')
+            );
+        }
+
+        const bcSubscriber = await createSubscriber(_args.email);
+
+        const subscribeEmailToNewsletterOutput = await transformSubscriberToNewsletterOutput(bcSubscriber);
+
+        return subscribeEmailToNewsletterOutput;
     },
 };
+
+export async function transformSubscriberToNewsletterOutput(bcSubscriber: BcSubscriber): Promise<SubscribeEmailToNewsletterOutput> {
+    console.log('In transformSubscriberToNewsletterOutput');
+    return {
+        status: 'SUBSCRIBED'
+    }
+}
