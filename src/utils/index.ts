5969f854451476d2d0028d778fb4c4ee3e6e08f3
Brett Cutting <brett.cutting@aligent.com.au>
2023-08-07 12:58:44 +1000

    MICRO-96: adds get-path-from-url-key test add get-path-from-url-key to index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './encode-decode';
 export * from './get-is-virtual-cart';
+export * from './get-path-from-url-key';
 export * from './get-url';
 export * from './regex/slash-at-start-or-end';
