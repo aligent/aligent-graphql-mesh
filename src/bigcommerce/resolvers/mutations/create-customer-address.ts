5819269fa226ed7cf446eee8d6a09d54ce59a177
Adam Hall <adam.hall@aligent.com.au>
2023-08-08 14:38:49 +0930

    MICRO-119: Add extended ValidatedInput type and validate function to check the input matches the minimum requirements for the BC Customer Address API

--- src/bigcommerce/resolvers/mutations/create-customer-address.ts
+++ src/bigcommerce/resolvers/mutations/create-customer-address.ts
@@ -1,4 +1,24 @@
-import { MutationResolvers } from '../../../meshrc/.mesh';
+export interface ValidatedInput extends CustomerAddressInput {
+    firstname: string,
+    lastname: string,
+    city: string,
+    country_code: CountryCodeEnum,
+    street: string[],
+    region: {
+        region: string
+    },
+    postcode: string
+}
+
+export const isCustomerAddressValid = (input: CustomerAddressInput): boolean => {
+   return !!(input.firstname &&
+       input.lastname &&
+       input.city &&
+       input.country_code &&
+       input.street &&
+       input.region?.region &&
+       input.postcode);
+}
 
 export const createCustomerAddressResolver: MutationResolvers['createCustomerAddress'] = {
     resolve: (_root, _args, _context, _info) => {
