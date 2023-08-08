8d5651feae759da06ed78f3d558a3ca683ccc940
Adam Hall <adam.hall@aligent.com.au>
2023-08-08 14:41:39 +0930

    MICRO-119: Add tests for transforms and validate functions

---
+++ src/bigcommerce/resolvers/mutations/tests/customer-address-transformation.test.ts
@@ -0,0 +1,14 @@
+import {bcAddress, customerAddress} from "./__data__/customer-address-data";
+import { transformCustomerAddress, transformBcAddress } from '../../../factories/transform-customer-address-data';
+
+describe('Customer Address Transformation tests', () => {
+    test('Transform CustomerAddress into BCAddress', () => {
+        const transformed = transformCustomerAddress(customerAddress, 123);
+        expect(transformed).toEqual(bcAddress);
+    });
+
+    test('Transform BCAddress into CustomerAddress', () => {
+        const transformed = transformBcAddress(bcAddress)
+        expect(transformed).toEqual(customerAddress);
+    });
+});
