bda51477ab1da07229ed5ffb9313212a995e2ef6
Adam Hall <adam.hall@aligent.com.au>
2023-08-10 10:46:51 +0930

    MICRO-109: Configure express server to use the cors package and send appropriate cors headers

--- docker/server.ts
+++ docker/server.ts
@@ -1,8 +1,38 @@
 import { createBuiltMeshHTTPHandler } from './.mesh';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+/*
+* Configure CORS and add middleware for use on all routes
+*/
+const corsConfiguration: cors.CorsOptions  = {
+    origin: process.env.ORIGIN,
+    credentials: true,
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'preview-version',
+        'x-recaptcha',
+        'mesh-token'
+    ]
+};
+app.use(cors(corsConfiguration));
+
+/*
+* Respond to all OPTIONS requests with CORS headers
+*/
+app.options('*', cors(corsConfiguration));
+
+/**
+ * Add GraphQL Mesh route
+ */
 app.use('/graphql', createBuiltMeshHTTPHandler());
+
+/**
+ * Load Balancer health check
+ */
 app.use('/healthcheck', (_req, res) => {
     return res.send('ok');
 });
