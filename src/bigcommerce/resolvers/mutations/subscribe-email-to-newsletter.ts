def26ac82082086176705d2dcf5839590db6c478
jan.plank <jan.plank@aligent.com.au>
2023-08-10 16:32:14 +0930

    MICRO-120: clean imports

--- src/bigcommerce/resolvers/mutations/subscribe-email-to-newsletter.ts
+++ src/bigcommerce/resolvers/mutations/subscribe-email-to-newsletter.ts
@@ -1,5 +1,4 @@
 import { MutationResolvers, SubscribeEmailToNewsletterOutput } from '@mesh';
-import { logAndThrowError } from '../../../utils/error-handling';
 import { createSubscriber } from '../../apis/rest/subscriber';
 import { BcSubscriber } from '../../types';
 
