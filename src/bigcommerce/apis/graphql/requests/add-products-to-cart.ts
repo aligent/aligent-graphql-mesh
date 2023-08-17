c1565cc860fb382a2536a5d0df41b69746d323a5
chamal <chamal.chamikara@aligent.com.au>
2023-08-17 10:33:06 +0930

    MICRO-107: Adding whole cart details to add to cart response

--- src/bigcommerce/apis/graphql/requests/add-products-to-cart.ts
+++ src/bigcommerce/apis/graphql/requests/add-products-to-cart.ts
@@ -1,22 +1,17 @@
 import { stripIgnoredCharacters } from 'graphql';
 import { gql } from 'graphql-tag';
 import { print } from 'graphql/index';
+import { cartDetails } from '../fragments/cartDetails';
 
 export const addProductsToCartMutation = stripIgnoredCharacters(
     print(gql`
+        ${cartDetails}
+
         mutation addProductsToCart($cartId: String!, $cartItems: AddCartLineItemsDataInput!) {
             cart {
                 addCartLineItems(input: { cartEntityId: $cartId, data: $cartItems }) {
                     cart {
-                        id
-                        entityId
-                        lineItems {
-                            physicalItems {
-                                quantity
-                                sku
-                                name
-                            }
-                        }
+                        ...CartDetails
                     }
                 }
             }
