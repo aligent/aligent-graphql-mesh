34763ea03f96ecdfc115158a60539b59598fc73a
jan.plank <jan.plank@aligent.com.au>
2023-08-17 16:20:28 +0930

    MICRO-139: upgrade code to match new logic
    - upgrade to new error handling
    - change address verification logic to improved version

--- src/bigcommerce/resolvers/mutations/delete-customer-address.ts
+++ src/bigcommerce/resolvers/mutations/delete-customer-address.ts
@@ -1,25 +1,21 @@
 import { MutationResolvers } from '@mesh';
-import { logAndThrowError } from '../../../utils/error-handling';
-import { getDecodedMeshToken } from '../../../utils/tokens';
-import { deleteCustomerAddress, getCustomerAddresses } from '../../apis/rest/customer';
-import { addressIdBelongsToCustomer } from '../../../utils/validators/customer-address-validator';
+import { deleteCustomerAddress, getCustomerAddress } from '../../apis/rest/customer';
+import { logAndThrowError } from '../../../utils/error-handling/error-handling';
+import { getBcCustomerIdFromMeshToken } from '../../../utils/tokens';
 
 export const deleteCustomerAddressResolver: MutationResolvers['deleteCustomerAddress'] = {
     resolve: async (_root, args, context, _info) => {
-        if (!context.headers['mesh-token']) {
-            return logAndThrowError(new Error('mesh-token header is required for this mutation.'));
-        }
-
-        const { bc_customer_id: customerId } = getDecodedMeshToken(context.headers['mesh-token']);
+        const customerId = getBcCustomerIdFromMeshToken(context.headers.authorization);
 
         const addressId = args.id;
 
-        const customerAddresses = await getCustomerAddresses(customerId);
-        if (!addressIdBelongsToCustomer(addressId, customerAddresses)) {
+        const customerAddress = await getCustomerAddress(addressId, customerId);
+        if (!customerAddress) {
             return logAndThrowError(
                 new Error('AuthorizationError: Address does not belong to customer')
             );
         }
+
         await deleteCustomerAddress(addressId);
         //Nothing is returned by BigComm, not matter if success or not, always 204 No Content
         //So if there is no critical error we are just returning null (TODO: Check with PWA if that's ok)
