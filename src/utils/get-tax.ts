1122a91e1f2962774a1b0015873d45b719faf74c
Brett Cutting <brett.cutting@aligent.com.au>
2023-08-17 16:39:10 +1000

    JOV-101: add getGstPercentBetweenPrices function to work out the gst between prices

--- src/utils/get-tax.ts
+++ src/utils/get-tax.ts
@@ -1,6 +1,12 @@
-import { BC_TaxPriceDisplay } from '@mesh/external/BigCommerceGraphqlApi';
+import { BC_Money, BC_TaxPriceDisplay } from '@mesh/external/BigCommerceGraphqlApi';
 
 export const getIncludesTax = (priceDisplay?: BC_TaxPriceDisplay | null): boolean => {
     if (!priceDisplay) return true;
     return ['INC', 'BOTH'].includes(priceDisplay);
+};
+
+export const getGstPercentBetweenPrices = (listPrice: BC_Money, salePrice: BC_Money): number => {
+    if (listPrice.value === salePrice.value) return 0;
+
+    return Math.round(((listPrice.value - salePrice.value) / salePrice.value) * 100);
 };
