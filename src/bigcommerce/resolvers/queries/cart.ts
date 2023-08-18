f60420fc3eb03825a66d907189b5d6fe0e4bb5f7
Brett Cutting <brett.cutting@aligent.com.au>
2023-08-18 20:25:55 +1000

    MICRO-143: get bcCustomerId from the getBcCustomerId function

--- src/bigcommerce/resolvers/queries/cart.ts
+++ src/bigcommerce/resolvers/queries/cart.ts
@@ -2,12 +2,11 @@ import { Cart, Maybe, QueryResolvers } from '@mesh';
 import { getCheckout } from '../../apis/graphql/checkout';
 import { getTransformedCartData } from '../../factories/transform-cart-data';
 import { GraphQlContext } from '../../../meshrc/types';
-import { getBcCustomerIdFromMeshToken } from '../../../utils/tokens';
+import { getBcCustomerId } from '../../../utils';
 
 export const cartResolver: QueryResolvers['cart'] = {
     resolve: async (_root, args, context: GraphQlContext, _info): Promise<Maybe<Cart>> => {
-        // const customerImpersonationToken = await context.cache.get('customerImpersonationToken');
-        const bcCustomerId = getBcCustomerIdFromMeshToken(context.headers['mesh-token']);
+        const bcCustomerId = getBcCustomerId(context);
 
         const response = await getCheckout(args.cart_id, bcCustomerId);
         if (!response?.entityId) return null;
