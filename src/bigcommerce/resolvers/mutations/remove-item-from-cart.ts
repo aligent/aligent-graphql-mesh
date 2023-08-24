cd0bc19bbcf3ea14bc48c5f748ff4d8f6a04b6f8
Brett Cutting <brett.cutting@aligent.com.au>
2023-08-24 18:53:50 +1000

    MICRO-148: update the removeItemFromCartResolver to grab the lineItemEntityId from the deconstructed cart_item_uid. cart_item_id is deprecated and will get updated in Take Flight return instead of throw an error in updateCartIdAttribute. This is preventing guest users have a cart created

--- src/bigcommerce/resolvers/mutations/remove-item-from-cart.ts
+++ src/bigcommerce/resolvers/mutations/remove-item-from-cart.ts
@@ -1,12 +1,23 @@
 import { MutationResolvers } from '@mesh';
 import { deleteCartLineItem } from '../../apis/graphql';
-import { getBcCustomerId, logAndThrowError } from '../../../utils';
+import { getBcCustomerId, getDeconstructedCartItemUid, logAndThrowError } from '../../../utils';
 import { getCheckout } from '../../apis/graphql';
 import { getTransformedCartData } from '../../factories/transform-cart-data';
 
+export const UNDEFINED_CART = {
+    id: '',
+    items: [],
+    total_quantity: 0,
+    available_gift_wrappings: [],
+    gift_receipt_included: false,
+    is_virtual: false,
+    printed_card_included: false,
+    shipping_addresses: [],
+};
+
 export const removeItemFromCartResolver: MutationResolvers['removeItemFromCart'] = {
     resolve: async (_root, args, context, _info) => {
-        if (!args.input?.cart_id || !args.input.cart_item_id) {
+        if (!args.input?.cart_id || !args.input.cart_item_uid) {
             return logAndThrowError('Missing cart id or cart item id');
         }
         const customerImpersonationToken = (await context.cache.get(
@@ -15,14 +26,27 @@ export const removeItemFromCartResolver: MutationResolvers['removeItemFromCart']
 
         const bcCustomerId = getBcCustomerId(context);
 
+        /**
+         * Extracts the "lineItemEntityId", "productEntityId" and "variantEntityId" properties from the "cartItemUid".
+         * The cartItemUid is initial formed via:
+         * @see createCartItemUid
+         */
+        const { lineItemEntityId, productEntityId, variantEntityId } = getDeconstructedCartItemUid(
+            args.input.cart_item_uid
+        );
+
         const removeCartItemResponse = await deleteCartLineItem(
             args.input.cart_id,
-            args.input.cart_item_id.toString(),
+            lineItemEntityId,
             customerImpersonationToken,
             bcCustomerId
         );
 
-        if (!removeCartItemResponse?.entityId) return null;
+        if (!removeCartItemResponse?.entityId)
+            return {
+                cart: UNDEFINED_CART,
+            };
+
         const checkoutResponse = await getCheckout(
             removeCartItemResponse.entityId,
             bcCustomerId,
