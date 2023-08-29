0e0f4fefd4a1c489aa5f97f24df3527d709989df
jack.mcloughlin <jackmaca@msn.com>
2023-08-29 11:02:19 +0930

    MICRO-144 change sku to be not encoded

--- src/bigcommerce/factories/transform-rest-cart-line-items.ts
+++ src/bigcommerce/factories/transform-rest-cart-line-items.ts
@@ -4,7 +4,7 @@ import { BCOrderLineItem } from '../types';
 export const transformRestCartLineItems = (lineItem: BCOrderLineItem): CartItemInput => ({
     quantity: lineItem.quantity,
     uid: btoa(`Product:${lineItem.product_id}`), // e.g. UHJvZHVjdDoxMDc= -> Product:107
-    sku: btoa(`Sku:${lineItem.sku}`), // e.g. U2t1OkRQQg== -> Sku:DPB
+    sku: lineItem.sku,
     selected_options: lineItem.product_options.map((option) => {
         return btoa(['configurable', option.option_id, option.product_option_id].join('/'));
     }),
