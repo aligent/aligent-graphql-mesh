84753f43f29fc3c921f9b62cacfe13d3253cdd12
Alan Chen <alan.chen@alig2219.staff.aligent.consulting>
2023-08-29 12:01:00 +0930

    MICRO-96: optimize countries resolver

--- src/bigcommerce/resolvers/queries/__tests__/countries-transformation.test.ts
+++ src/bigcommerce/resolvers/queries/__tests__/countries-transformation.test.ts
@@ -1,32 +1,34 @@
 import { CountryStates } from '../../../types';
-import { transformCountriesStates, transformCountry } from '../countries';
+import { transformCountriesAndStates } from '../countries';
 import { bcCountry, bcStates } from './__data__/countries-input-data';
 import { transformedCountries, transformedStates } from './__data__/countries-transformed-data';
 
 describe('Countries data transform tests', () => {
     test('return transformed states', () => {
+        const inputBcCountry = [bcCountry];
         const inputBcStates = bcStates;
         const inputTransformedStates = transformedStates;
 
-        const transformed = transformCountriesStates(inputBcStates);
+        const transformed = transformCountriesAndStates(inputBcCountry, inputBcStates);
 
-        expect(transformed).toEqual(inputTransformedStates);
+        expect(transformed[0].available_regions).toEqual(inputTransformedStates);
     });
 
     test('return null when no states for a country', () => {
+        const inputBcCountry = [bcCountry];
         const inputNoStates: CountryStates[] = [];
 
-        const transformed = transformCountriesStates(inputNoStates);
+        const transformed = transformCountriesAndStates(inputBcCountry, inputNoStates);
 
-        expect(transformed).toEqual(null);
+        expect(transformed[0].available_regions).toEqual([]);
     });
 
     test('return transformed countries', () => {
-        const inputBcCountry = bcCountry;
+        const inputBcCountry = [bcCountry];
         const inputBcStates = bcStates;
-        const inputTransformedCountries = transformedCountries;
+        const inputTransformedCountries = [transformedCountries];
 
-        const transformed = transformCountry(inputBcCountry, inputBcStates);
+        const transformed = transformCountriesAndStates(inputBcCountry, inputBcStates);
 
         expect(transformed).toEqual(inputTransformedCountries);
     });
