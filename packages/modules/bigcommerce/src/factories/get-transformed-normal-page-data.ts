0c3c9399eddabb642975db3f1f825d979e27e09a
chamal <chamal.chamikara@aligent.com.au>
2023-09-07 14:26:05 +0930

    MICRO-131: Add a comment to explain redirect_code

--- packages/modules/bigcommerce/src/factories/get-transformed-normal-page-data.ts
+++ packages/modules/bigcommerce/src/factories/get-transformed-normal-page-data.ts
@@ -12,6 +12,8 @@ export const getTransformedNormalPageData = (data: NormalPage): CmsPage => {
         meta_title: seo.pageTitle,
         meta_keywords: seo.metaKeywords,
         meta_description: seo.metaDescription,
+        // Since cmsPage type has extended from RoutableInterface where redirect_code is required we are just mocking as 0
+        // 0 means there is no redirect error, this will be fine as TF doesn't use redirect_code in cmsPage query at all
         redirect_code: 0,
     };
 };
