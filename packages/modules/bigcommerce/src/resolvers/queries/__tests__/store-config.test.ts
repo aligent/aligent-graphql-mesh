bb0c2b4ad38bb70f7dddfbbbd1ae51d133cfb11e
Brett Cutting <brett.cutting@aligent.com.au>
2023-09-08 17:47:05 +1000

    MICRO-182: update the store config resolver to return all pwa_config data. update the types of the store configs we know about add tests for the getConfigsFromMetafields function

--- packages/modules/bigcommerce/src/resolvers/queries/__tests__/store-config.test.ts
+++ packages/modules/bigcommerce/src/resolvers/queries/__tests__/store-config.test.ts
@@ -1,6 +1,10 @@
 import { MetafieldEdge, MetafieldConnection } from '@aligent/bigcommerce-operations';
 import { StoreConfig } from '@aligent/bigcommerce-resolvers';
-import { findMetafieldValueByKey, transformChannelMetafieldsToStoreConfig } from '../store-config';
+import {
+    findMetafieldValueByKey,
+    getConfigsFromMetafields,
+    transformChannelMetafieldsToStoreConfig,
+} from '../store-config';
 
 describe('Transform StoreConfig tests', () => {
     test('Mandatory StoreConfig exits', async () => {
@@ -39,6 +43,136 @@ describe('Transform StoreConfig tests', () => {
         const value: string = findMetafieldValueByKey(data, 'some');
 
         expect(value).toEqual('');
+    });
+
+    test('Parses a boolean string to a boolean value', async () => {
+        const data: MetafieldEdge[] = [
+            {
+                // @ts-expect-error: Type expects `entityId`
+                node: {
+                    id: 'TWV0YWZpZWxkczoxODk=',
+                    key: 'newsletter_enabled',
+                    value: 'false',
+                },
+            },
+        ];
+        getConfigsFromMetafields(data);
+
+        const expectedResult = { newsletter_enabled: false };
+
+        expect(getConfigsFromMetafields(data)).toEqual(expectedResult);
+    });
+
+    test('Parses a number string to a boolean value', async () => {
+        const data: MetafieldEdge[] = [
+            {
+                // @ts-expect-error: Type expects `entityId`
+                node: {
+                    id: 'TWV0YWZpZWxkczoxODk=',
+                    key: 'newsletter_enabled',
+                    value: '1',
+                },
+            },
+        ];
+        getConfigsFromMetafields(data);
+
+        const expectedResult = { newsletter_enabled: true };
+
+        expect(getConfigsFromMetafields(data)).toEqual(expectedResult);
+    });
+
+    test('Parses a JSON string value to an object value', async () => {
+        const data: MetafieldEdge[] = [
+            {
+                // @ts-expect-error: Type expects `entityId`
+                node: {
+                    id: 'TWV0YWZpZWxkczoxODk=',
+                    key: 'send_friend',
+                    value: '{"enabled_for_customers": true, "enabled_for_guests": false}',
+                },
+            },
+        ];
+        getConfigsFromMetafields(data);
+
+        const expectedResult = {
+            send_friend: { enabled_for_customers: true, enabled_for_guests: false },
+        };
+
+        expect(getConfigsFromMetafields(data)).toEqual(expectedResult);
+    });
+
+    test('Parses a bad JSON string value to a null value', async () => {
+        const data: MetafieldEdge[] = [
+            {
+                // @ts-expect-error: Type expects `entityId`
+                node: {
+                    id: 'TWV0YWZpZWxkczoxODk=',
+                    key: 'newsletter_enabled',
+                    value: '1',
+                },
+            },
+            {
+                // @ts-expect-error: Type expects `entityId`
+                node: {
+                    id: 'TWV0YWZpZWxkczoxODk=',
+                    key: 'send_friend',
+                    value: '{enabled_for_customers: true, enabled_for_guests: false}',
+                },
+            },
+        ];
+        getConfigsFromMetafields(data);
+
+        const expectedResult = {
+            newsletter_enabled: true,
+            send_friend: null,
+        };
+
+        expect(getConfigsFromMetafields(data)).toEqual(expectedResult);
+    });
+
+    test(`Returns {} if there's no metafields`, async () => {
+        const data = undefined;
+        getConfigsFromMetafields(data);
+
+        const expectedResult = {};
+
+        expect(getConfigsFromMetafields(data)).toEqual(expectedResult);
+    });
+
+    test(`Returns {} if there's no key`, async () => {
+        const data: MetafieldEdge[] = [
+            {
+                // @ts-expect-error: Type expects `entityId`
+                node: {
+                    id: 'TWV0YWZpZWxkczoxODk=',
+                    key: '',
+                    value: '',
+                },
+            },
+        ];
+        getConfigsFromMetafields(data);
+
+        const expectedResult = {};
+
+        expect(getConfigsFromMetafields(data)).toEqual(expectedResult);
+    });
+
+    test(`Returns a null value if there's no property value`, async () => {
+        const data: MetafieldEdge[] = [
+            {
+                // @ts-expect-error: Type expects `entityId`
+                node: {
+                    id: 'TWV0YWZpZWxkczoxODk=',
+                    key: 'grid_per_page',
+                    value: '',
+                },
+            },
+        ];
+        getConfigsFromMetafields(data);
+
+        const expectedResult = {};
+
+        expect(getConfigsFromMetafields(data)).toEqual(expectedResult);
     });
 });
 
--- packages/modules/bigcommerce/src/resolvers/queries/store-config.ts
+++ packages/modules/bigcommerce/src/resolvers/queries/store-config.ts
@@ -37,26 +37,20 @@ export async function transformChannelMetafieldsToStoreConfig(
     //The metafields data has this ane extra node attribute and needs to be accessed via node.node
     ///[{"node":{"id":"TWV0YWZpZWxkczoxODk=","key":"category_url_suffix","value":".html"}},{"node":{"id":"TWV0YWZpZWxkczoxOTA=","key":"grid_per_page","value":"24"}}]
 
+    const configs = getConfigsFromMetafields(metafields);
+
     const storeConfigTransformed: StoreConfig = {
         //Mandatory fields, always returned (currently no value assigned)
         contact_enabled: false,
         newsletter_enabled: false,
         pwa_base_url: '',
         returns_enabled: '',
+        root_category_uid: 'null',
+        locale: 'en-AU',
+        category_url_suffix: '',
+        grid_per_page: 24,
+        ...configs,
     };
-
-    if (metafields) {
-        const categoryUrl: string = findMetafieldValueByKey(metafields, 'category_url_suffix');
-        const gridPerPage: string = findMetafieldValueByKey(metafields, 'grid_per_page');
-        const locale: string = findMetafieldValueByKey(metafields, 'locale');
-        const root_category_uid: string = findMetafieldValueByKey(metafields, 'root_category_uid');
-        //Add more metafields as required here. Metafields need to be added to bigcommerce manually first.
-
-        storeConfigTransformed.category_url_suffix = categoryUrl;
-        storeConfigTransformed.grid_per_page = parseInt(gridPerPage !== '' ? gridPerPage : '24'); // default set to 24
-        storeConfigTransformed.locale = locale || null;
-        storeConfigTransformed.root_category_uid = root_category_uid || null;
-    }
 
     return storeConfigTransformed;
 }
@@ -70,3 +64,85 @@ export function findMetafieldValueByKey(
     });
     return metafieldValue ? metafieldValue.node?.value : '';
 }
+
+const booleanConfig = [
+    'adyen_demo_mode',
+    'adyen_has_holder_name',
+    'adyen_holder_name_required',
+    'autocomplete_on_storefront',
+    'check_money_order_enable_for_specific_countries',
+    'check_money_order_enabled',
+    'contact_enabled',
+    'newsletter_enabled',
+    'returns_enabled',
+    'use_store_in_url',
+    'zero_subtotal_enable_for_specific_countries',
+];
+
+const integerConfig = [
+    'check_money_order_sort_order',
+    'demonotice',
+    'grid_per_page',
+    'list_per_page',
+    'show_cms_breadcrumbs',
+    'store_sort_order',
+];
+
+const jsonStringConfig = [
+    'configurable_thumbnail_source',
+    'category_fixed_product_tax_display_setting',
+    'product_fixed_product_tax_display_setting',
+    'sales_fixed_product_tax_display_setting',
+    'send_friend',
+    'paypal_express',
+    'StoreInformation',
+];
+
+/**
+ * de-nests channel metafields into key/value pairs and parses the value to be its intended type
+ *
+ * e.g. [{"node":{"id":"TWV0YWZpZWxkczoxODk=","key":"category_url_suffix","value":".html"}},{"node":{"id":"TWV0YWZpZWxkczoxOTA=","key":"grid_per_page","value":"24"}}]
+ * =
+ * {
+ *  category_url_suffix: ".html",
+ *  grid_per_page: "24"
+ * }
+ *
+ * @param metafields
+ */
+export const getConfigsFromMetafields = (
+    metafields?: Maybe<Array<Maybe<MetafieldEdge>>>
+): { [key: string]: boolean | number | string } => {
+    if (!metafields) return {};
+
+    return metafields.reduce((carry, metafield) => {
+        if (!metafield?.node.key) return carry;
+        const { key } = metafield.node;
+
+        let value: any = metafield.node.value;
+
+        if (!value) return carry;
+
+        if (integerConfig.includes(key)) {
+            value = Number(value);
+        }
+
+        if (booleanConfig.includes(key)) {
+            if (isNaN(value)) {
+                value = !(value === 'false');
+            } else {
+                value = !!Number(value);
+            }
+        }
+
+        if (jsonStringConfig.includes(key)) {
+            try {
+                value = JSON.parse(value);
+            } catch {
+                value = null;
+            }
+        }
+
+        return { ...carry, [key]: value };
+    }, {});
+};
