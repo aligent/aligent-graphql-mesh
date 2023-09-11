46bcbd8ecf83f1a5deb773a6b8da16906be6aaf3
Brett Cutting <brett.cutting@aligent.com.au>
2023-09-12 09:40:58 +1000

    MICRO-190: Throw an AuthorizationError in the login mutation if either the entityId or result is not successful

--- packages/modules/bigcommerce/src/apis/graphql/login.ts
+++ packages/modules/bigcommerce/src/apis/graphql/login.ts
@@ -1,4 +1,4 @@
-import { logAndThrowError } from '@aligent/utils';
+import { AuthorizationError, logAndThrowError } from '@aligent/utils';
 import { bcGraphQlRequest } from './client';
 import { loginMutation } from './requests/login';
 
@@ -24,8 +24,10 @@ export const bcLogin = async (
     const entityId = response.data?.login.customer.entityId;
     const result = response.data?.login.result;
 
-    if (result !== 'success') {
-        return logAndThrowError(`Failed to authenticate with BigCommerce`);
+    if (!entityId || result !== 'success') {
+        throw new AuthorizationError(
+            `The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.`
+        );
     }
 
     return entityId;
