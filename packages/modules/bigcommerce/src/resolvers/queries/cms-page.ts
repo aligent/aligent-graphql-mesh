57c57bcd4d7cef71b535dbafcc9f84d951000bfd
chamal <chamal.chamikara@aligent.com.au>
2023-09-18 09:00:18 +0930

    MICRO-131: Change the comment slightly

--- packages/modules/bigcommerce/src/resolvers/queries/cms-page.ts
+++ packages/modules/bigcommerce/src/resolvers/queries/cms-page.ts
@@ -8,8 +8,8 @@ export const cmsPageResolver = {
             return logAndThrowError('Required field identifier is missing');
         }
 
-        // In big commerce all the URLs have slashes in start and end of the URL key but TF sometimes send
-        // slashes only at beginning sometimes both and sometimes only at end, so standardising it by removing all
+        //In big commerce, all the URLs have slashes at the start and end of the URL key, but TF sometimes sends slashes only at the beginning,
+        // sometimes at both ends, and sometimes only at the end, so standardizing them by removing all
         // the slashes and re adding slashes to start and end
         const urlWithoutSlashes = args.identifier.replace(/\//g, '');
         const url = `/${urlWithoutSlashes}/`;
