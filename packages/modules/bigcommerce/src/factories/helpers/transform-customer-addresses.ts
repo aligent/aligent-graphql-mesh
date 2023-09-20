9710bc273122300be80e377898a44db1cc997cd2
Silvia Taliana <silvia.taliana@alig2218.staff.aligent.consulting>
2023-09-20 14:25:00 +0930

    MICRO-203: format fix

--- packages/modules/bigcommerce/src/factories/helpers/transform-customer-addresses.ts
+++ packages/modules/bigcommerce/src/factories/helpers/transform-customer-addresses.ts
@@ -16,7 +16,7 @@ export const getTransformedCustomerAddresses = (
             postal_code,
             country_code,
             phone,
-            form_fields
+            form_fields,
         } = address;
         return {
             id: id,
@@ -28,7 +28,9 @@ export const getTransformedCustomerAddresses = (
             region: {
                 region: state_or_province,
                 region_code: null, // BC doesnt return this
-                region_id: form_fields.find(field => field.name === 'region_id')?.value as number || null
+                region_id:
+                    (form_fields.find((field) => field.name === 'region_id')?.value as number) ||
+                    null,
             },
             postcode: postal_code,
             country_code: country_code as CountryCodeEnum,
