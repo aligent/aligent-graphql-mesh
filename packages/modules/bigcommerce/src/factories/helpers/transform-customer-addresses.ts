c1f454ebcafe4415b8ea4a9362d3e48b884e18f0
Silvia Taliana <silvia.taliana@alig2218.staff.aligent.consulting>
2023-09-20 15:44:12 +0930

    MICRO-207: adding region code to customer address query

--- packages/modules/bigcommerce/src/factories/helpers/transform-customer-addresses.ts
+++ packages/modules/bigcommerce/src/factories/helpers/transform-customer-addresses.ts
@@ -27,7 +27,9 @@ export const getTransformedCustomerAddresses = (
             street: [address1],
             region: {
                 region: state_or_province,
-                region_code: null, // BC doesnt return this
+                region_code: 
+                    (form_fields.find((field) => field.name === 'region_code')?.value as string) ||
+                    '',
                 region_id:
                     (form_fields.find((field) => field.name === 'region_id')?.value as number) ||
                     null,
