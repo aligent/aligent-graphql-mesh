20ed432581daf78ae5d734a9b25ea8003782efd3
Brett Cutting <brett.cutting@aligent.com.au>
2023-09-22 14:24:23 +1000

    MICRO-214: put a cap on the number of skus we can query return null if the number of product ids from the rest products query is 0

--- packages/modules/bigcommerce/src/resolvers/queries/get-products-by-sku.ts
+++ packages/modules/bigcommerce/src/resolvers/queries/get-products-by-sku.ts
@@ -3,9 +3,12 @@ import { getTransformedProductsData } from '../../factories/transform-products-d
 import { getTaxSettings } from '../../apis/graphql/settings';
 import { getProducts } from '../../apis/rest/products';
 import { getBcProductsGraphql } from '../../apis/graphql';
-import { getIncludesTax } from '@aligent/utils';
+import { AxiosGraphqlError, getIncludesTax } from '@aligent/utils';
 import { transformGQLSortArgsToRestSortArgs } from '../../../../../utils/sort';
 import { Maybe, ProductEdge } from '@aligent/bigcommerce-operations';
+
+/* This is the maximum page size we're allowing for a products by sku look up */
+const MAX_SKU_QUERY_LIMIT = 50;
 
 /* A mapping how we want sort args property names translated to names the products
  * rest GET can work with.
@@ -36,6 +39,10 @@ export const productsBySkuResolver: QueryResolvers['productsBySku'] = {
 
         const skuLength = uniqueSkus.length;
 
+        if (skuLength > MAX_SKU_QUERY_LIMIT) {
+            throw new AxiosGraphqlError('Cannot have more than 50 skus');
+        }
+
         /* This whole query is based on getting products by sku so if we don't have an array of skus
          * at this point return null*/
         if (skuLength === 0) return null;
@@ -53,7 +60,11 @@ export const productsBySkuResolver: QueryResolvers['productsBySku'] = {
         if (!productsBySku?.data) return null;
 
         /* Collect all the rest products ids. They should be in the correct sort order */
-        const productIds = productsBySku?.data.map((product) => product.id);
+        const productIds = productsBySku?.data.map((product) => product.id) || [];
+
+        /* If we pass an empty array to the graphql products query it will return all products, so
+         * return null instead */
+        if (productIds.length === 0) return null;
 
         /* Query for the products through the graphql products query with the collected product ids from the
          * rest GET request.
