a8991e3b4db11313fbf03e3239260283e4b5e1bc
Adam Hall <adam.hall@aligent.com.au>
2023-09-28 12:18:08 +0930

    Remove Oro module from bigcommerce mesh

--- packages/mesh/bigcommerce/application.ts
+++ packages/mesh/bigcommerce/application.ts
@@ -1,19 +1,8 @@
-import { Module, createApplication } from 'graphql-modules';
+import { createApplication } from 'graphql-modules';
 import { createBigCommerceModule } from '@aligent/bigcommerce-graphql-module';
-import { createOroCommerceModule } from '@aligent/orocommerce-graphql-module';
 
-const modules: Module[] = [];
-
-if (process.env.MODE == 'ORO') {
-    modules.push(
-        createOroCommerceModule({
-            storeUrl: process.env.ORO_STORE_URL as string,
-            clientId: process.env.ORO_CLIENT_ID as string,
-            clientSecret: process.env.ORO_CLIENT_SECRET as string,
-        })
-    );
-} else {
-    modules.push(
+export default createApplication({ 
+    modules: [
         createBigCommerceModule({
             graphqlEndpoint: process.env.BC_GRAPHQL_API as string,
             authToken: process.env.X_AUTH_TOKEN as string,
@@ -22,7 +11,5 @@ if (process.env.MODE == 'ORO') {
             clientId: process.env.BC_CLIENT_ID as string,
             storeHash: process.env.STORE_HASH as string,
         })
-    );
-}
-
-export default createApplication({ modules });
+    ]
+ });
