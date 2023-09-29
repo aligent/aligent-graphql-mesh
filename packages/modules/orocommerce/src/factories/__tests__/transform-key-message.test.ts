e977d268984b902b499aec352b119bd013b597fe
Bruno Pasqualini <bruno.pasqualini@aligent.com.au>
2023-09-29 16:20:35 +0930

    OTF-58: Add unit tests for transformKeyMessages

---
+++ packages/modules/orocommerce/src/factories/__tests__/transform-key-message.test.ts
@@ -0,0 +1,26 @@
+import { getMockKeyMessages } from "../../apis/rest/mocks/key-messages";
+import { transformKeyMessages } from "../transform-key-message";
+
+describe('Key Messages data transformation tests', () => {
+    const mockMessages = getMockKeyMessages();
+    const output = transformKeyMessages(mockMessages);
+
+    test('Check whether the transformed messages contains the expected properties', () => {
+        expect(output).toHaveProperty('enabled')
+        expect(output).toHaveProperty('messages')
+        expect(output).toHaveProperty('messages[0].link')
+        expect(output).toHaveProperty('messages[0].message')
+    });
+
+    test('Check whether the transformed messages contains expected data', () => {
+        expect(output.enabled).toBe(true)
+        expect(output.__typename).toBe('KeyMessageResult')
+
+        for (const msgIndex in output.messages!) {
+            const transformedMsg = output.messages[msgIndex]!;
+
+            expect(transformedMsg.link).toBe(mockMessages[msgIndex].link)
+            expect(transformedMsg.message).toBe(mockMessages[msgIndex].message)
+        }
+    });
+});
--- packages/modules/orocommerce/src/factories/transform-key-message.ts
+++ packages/modules/orocommerce/src/factories/transform-key-message.ts
@@ -3,9 +3,11 @@ import { KeyMessage as OroKeyMessage } from '../types/key-messages';
 
 export const transformKeyMessages = (keyMessages: OroKeyMessage[]): KeyMessageResult => {
     return {
+        __typename: 'KeyMessageResult',
         enabled: true,
         messages: keyMessages.map((message: OroKeyMessage): KeyMessage => {
             return {
+                __typename: 'KeyMessage',
                 link: message.link,
                 message: message.message,
             };
