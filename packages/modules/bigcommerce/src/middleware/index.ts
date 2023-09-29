1dd95a353ce70b9fe2444868ff3c4712a864859f
Daniel Van Der Ploeg <daniel.vanderploeg@aligent.com.au>
2023-09-29 10:22:53 +0930

    chore MICRO-223: fix formatting

--- packages/modules/bigcommerce/src/middleware/index.ts
+++ packages/modules/bigcommerce/src/middleware/index.ts
@@ -3,13 +3,15 @@ import axios from 'axios';
 
 const middlewareMap: MiddlewareMap = {
     '*': {
-        '*': [(context, next) => {
-            // Copy the x-forwarded-for header and send it with all axios requests
-            const forwardedIp = context.context.headers['x-forwarded-for'];
-            axios.defaults.headers.common['x-forwarded-for'] = forwardedIp;
-            return next();
-        }]
-    }
+        '*': [
+            (context, next) => {
+                // Copy the x-forwarded-for header and send it with all axios requests
+                const forwardedIp = context.context.headers['x-forwarded-for'];
+                axios.defaults.headers.common['x-forwarded-for'] = forwardedIp;
+                return next();
+            },
+        ],
+    },
 };
 
 export default middlewareMap;
