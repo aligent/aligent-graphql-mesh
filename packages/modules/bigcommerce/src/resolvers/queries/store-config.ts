cf2de8bbe2a816568ab67590d5dfbb2241cb4834
Brett Cutting <brett.cutting@aligent.com.au>
2023-10-05 09:08:51 +1000

    MICRO-225: add a checkMaintenanceMode function which checks if the site is in maintenance mode and make restricted incoming ip addresses throw an error

--- packages/modules/bigcommerce/src/resolvers/queries/store-config.ts
+++ packages/modules/bigcommerce/src/resolvers/queries/store-config.ts
@@ -2,6 +2,7 @@ import { MetafieldConnection } from '@aligent/bigcommerce-operations';
 import { QueryResolvers, StoreConfig } from '@aligent/bigcommerce-resolvers';
 import { getChannelMetafields } from '../../apis/graphql/channel';
 import { getAttributesFromMetaAndCustomFields } from '../../../../../utils/metafields';
+import { checkMaintenanceMode } from '../../apis/helpers/maintenance-mode';
 import {
     booleanStoreConfigProperties,
     integerStoreConfigProperties,
@@ -16,6 +17,12 @@ export const storeConfigResolver: QueryResolvers['storeConfig'] = {
         const customerImpersonationToken = (await context.cache.get(
             'customerImpersonationToken'
         )) as string;
+
+        /* Check if maintenance mode has been raised. If it has, an error will be thrown.
+         * The PWA calls the storeConfig query on page load and if the response is an error, it will
+         * raise the maintenance page. */
+        await checkMaintenanceMode(customerImpersonationToken, context);
+
         //The namespace needs to match the metafield namespace when created in BigCommerce
         const bcChannelMetafieldsConfig: MetafieldConnection = await getChannelMetafields(
             NAMESPACE,
