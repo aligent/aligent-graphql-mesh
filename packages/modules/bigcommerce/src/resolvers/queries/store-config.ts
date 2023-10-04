3670a95c8039ec06f841cb69810984718ab74903
Brett Cutting <brett.cutting@aligent.com.au>
2023-10-05 09:27:01 +1000

    MICRO-255: move the store-config transformation logic from the store-config resolver into a transform-store-configs file

--- packages/modules/bigcommerce/src/resolvers/queries/store-config.ts
+++ packages/modules/bigcommerce/src/resolvers/queries/store-config.ts
@@ -1,17 +1,11 @@
 import { MetafieldConnection } from '@aligent/bigcommerce-operations';
-import { QueryResolvers, StoreConfig } from '@aligent/bigcommerce-resolvers';
+import { QueryResolvers } from '@aligent/bigcommerce-resolvers';
 import { getChannelMetafields } from '../../apis/graphql/channel';
-import { getAttributesFromMetaAndCustomFields } from '../../../../../utils/metafields';
 import { checkMaintenanceMode } from '../../apis/helpers/maintenance-mode';
-import {
-    booleanStoreConfigProperties,
-    integerStoreConfigProperties,
-    jsonStringStoreConfigProperties,
-} from './constants';
+import { getTransformedChannelMetafieldsToStoreConfig } from '../../factories/transform-store-configs';
 
 const NAMESPACE: string = 'pwa_config';
 
-/* istanbul ignore next */
 export const storeConfigResolver: QueryResolvers['storeConfig'] = {
     resolve: async (_root, _args, context, _info) => {
         const customerImpersonationToken = (await context.cache.get(
@@ -29,46 +23,7 @@ export const storeConfigResolver: QueryResolvers['storeConfig'] = {
             customerImpersonationToken
         );
 
-        const storeConfig =
-            await transformChannelMetafieldsToStoreConfig(bcChannelMetafieldsConfig);
+        const storeConfig = getTransformedChannelMetafieldsToStoreConfig(bcChannelMetafieldsConfig);
         return storeConfig;
     },
 };
-
-/*
- * Metafields have to be added to BigCommerce manually on channel level.
- * Make sure the namespace is matching the NAMESPACE constant.
- * Use the Rest API or the Metafield Manger (if installed).
- * API endpoint:  https://api.bigcommerce.com/stores/{{store_hash}}/v3/channels/1/metafields
- * Docs: https://developer.bigcommerce.com/docs/rest-management/channels/channel-metafields#create-a-channel-metafield
- */
-export async function transformChannelMetafieldsToStoreConfig(
-    bcStoreConfig: MetafieldConnection
-): Promise<StoreConfig> {
-    const metafields = bcStoreConfig.edges;
-
-    //The metafields data has this ane extra node attribute and needs to be accessed via node.node
-    ///[{"node":{"id":"TWV0YWZpZWxkczoxODk=","key":"category_url_suffix","value":".html"}},{"node":{"id":"TWV0YWZpZWxkczoxOTA=","key":"grid_per_page","value":"24"}}]
-
-    const configs = getAttributesFromMetaAndCustomFields(metafields, {
-        booleanProperties: booleanStoreConfigProperties,
-        integerProperties: integerStoreConfigProperties,
-        jsonStringProperties: jsonStringStoreConfigProperties,
-    });
-
-    const storeConfigTransformed: StoreConfig = {
-        // Mandatory fields, always returned with no value assigned but configs specified via "store_config" metafields
-        // can override
-        contact_enabled: false,
-        newsletter_enabled: false,
-        pwa_base_url: '',
-        returns_enabled: '',
-        root_category_uid: 'null',
-        locale: 'en-AU',
-        category_url_suffix: '',
-        grid_per_page: 24,
-        ...configs,
-    };
-
-    return storeConfigTransformed;
-}
