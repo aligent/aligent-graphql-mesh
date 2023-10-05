0c8d195d121611670a074d6a96355b58639b1cf2
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2023-10-05 10:38:05 +1030

    MICRO-227: Apply middleware only to Query and Mutation

--- packages/modules/bigcommerce/src/middleware/index.ts
+++ packages/modules/bigcommerce/src/middleware/index.ts
@@ -1,19 +1,22 @@
-import { MiddlewareMap } from 'graphql-modules/shared/middleware';
+import { Middleware, MiddlewareMap } from 'graphql-modules/shared/middleware';
 import axios from 'axios';
 
+const addIpAddressToAxiosHeaders: Middleware = (context, next) => {
+    // Copy the x-forwarded-for header and send it with all axios requests
+    const forwardedIps = context.context.headers['x-forwarded-for'];
+    if (forwardedIps) {
+        const clientIp = forwardedIps.split(',').shift() || '';
+        axios.defaults.headers.common['x-forwarded-for'] = clientIp;
+    }
+    return next();
+};
+
 const middlewareMap: MiddlewareMap = {
-    '*': {
-        '*': [
-            (context, next) => {
-                // Copy the x-forwarded-for header and send it with all axios requests
-                const forwardedIps = context.context.headers['x-forwarded-for'];
-                if (forwardedIps) {
-                    const clientIp = forwardedIps.split(',').shift() || '';
-                    axios.defaults.headers.common['x-forwarded-for'] = clientIp;
-                }
-                return next();
-            },
-        ],
+    Query: {
+        '*': [addIpAddressToAxiosHeaders],
+    },
+    Mutation: {
+        '*': [addIpAddressToAxiosHeaders],
     },
 };
 
