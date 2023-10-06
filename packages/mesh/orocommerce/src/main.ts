8a045f1902f287c859d96ef11f45df0d68ff1fcd
Adam Hall <adam.hall@aligent.com.au>
2023-10-06 15:41:30 +1030

    Add express server to yoga so we can reuse CORS code

--- packages/mesh/orocommerce/src/main.ts
+++ packages/mesh/orocommerce/src/main.ts
@@ -1,12 +1,83 @@
-import { createServer } from 'node:http';
+import express from 'express';
+import cors from 'cors';
+import https from 'node:https';
 import { createYoga } from 'graphql-yoga';
 import { useGraphQLModules } from '@envelop/graphql-modules';
 import application from './application';
+import { readFileSync } from 'node:fs';
 
-const yoga = createYoga({ plugins: [useGraphQLModules(application)], graphiql: true });
-const server = createServer(yoga);
-const port = 4000 || process.env.PORT;
+const DEV_MODE = process.env?.NODE_ENV == 'development'; 
 
-server.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+const yoga = createYoga({ 
+    plugins: [useGraphQLModules(application)], 
+    graphiql: DEV_MODE,
+    logging: DEV_MODE ? 'info' : 'warn',
+    landingPage: false
 });
+
+const app = express();
+
+let allowedOrigins: (string | RegExp)[] = [
+    new RegExp('.*.dev.aligent.consulting$'),
+    new RegExp('.*.local.pwadev$'),
+];
+
+if (process.env.ORIGINS) {
+    const origins = process.env.ORIGINS.split(',');
+    allowedOrigins = allowedOrigins.concat(origins);
+}
+
+/*
+ * Configure CORS and add middleware for use on all routes
+ */
+const corsConfiguration: cors.CorsOptions = {
+    origin: allowedOrigins,
+    credentials: true,
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'preview-version',
+        'x-recaptcha',
+        'store',
+    ],
+};
+app.use(cors(corsConfiguration));
+
+/*
+ * Respond to all OPTIONS requests with CORS headers
+ */
+app.options('*', cors(corsConfiguration));
+
+/**
+ * Add GraphQL Mesh route
+ */
+app.use(yoga.graphqlEndpoint, yoga);
+
+/**
+ * Load Balancer health check
+ */
+app.use('/healthcheck', (_req, res) => {
+    return res.send('ok');
+});
+
+// Disable powered by header
+app.disable('x-powered-by')
+
+const port = 4000 || process.env.PORT;
+if (DEV_MODE) {
+    const server = https.createServer(
+        {
+            key: readFileSync('./certificates/cert.key'),
+            cert: readFileSync('./certificates/cert.crt')
+        }, 
+        app
+    );
+    
+    server.listen(port, () => {
+        console.log(`Mesh listening at https://localhost:${port}/graphql`);
+    });
+} else {
+    app.listen(port, () => {
+        console.log(`Mesh listening at https://localhost:${port}/graphql`);
+    });
+}
