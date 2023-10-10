62492dedaffc22068353930ba4311fffab9f7208
Adam Hall <adam.hall@aligent.com.au>
2023-10-11 09:54:46 +1030

    Add product operations to cachable objects

--- packages/mesh/cache.ts
+++ packages/mesh/cache.ts
@@ -10,22 +10,22 @@ interface CachableObjects {
 
 export default {
     operations: {
+        // Store config TTLs are set low as there is a maintenance flag check here
         getStoreConfig: 30,
+        storeConfigData: 30,
+
         getCountries: 86400,
         getSocialLinks: 1800,
         fullNavigationMenu: 600,
-        getProductsBySku: 600,
-        storeConfigData: 30,
         getBreadcrumbData: 600,
 
-        // @TODO: Enable after a lot more testing
-        // productDetail: 300,
+        getProductsBySku: 120,
+        productDetail: 120,
     },
     rules: [
-        // @TODO: Enable after a lot more testing
-        // {
-        //     pattern: /ResolveURL/,
-        //     maxAge: 300
-        // }
+        {
+            pattern: /ResolveURL/,
+            maxAge: 120
+        }
     ],
 } as CachableObjects;
