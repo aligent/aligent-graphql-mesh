51b0ec9da281c7bcb8a558c01bb35ce938203938
jack.mcloughlin <jackmaca@msn.com>
2023-10-18 11:01:22 +1030

    MICRO-138 add unit tests for time and date utils

--- packages/utils/__tests__/time-and-date.test.ts
+++ packages/utils/__tests__/time-and-date.test.ts
@@ -1,8 +1,32 @@
-import { convertDateFormat } from '../time-and-date';
+import {
+    convertDateFormat,
+    getUnixTimeStampInSeconds,
+    getUnixTimeStampInSecondsForMidnightTonight,
+} from '../time-and-date';
 
 describe('Time and Date tests', () => {
-    test('return date conveted from BC format to AC', () => {
+    test('return date converted from BC format to AC', () => {
         const result = convertDateFormat('Tue, 29 Aug 2023 02:26:57 +0000');
         expect(result).toEqual('2023-08-29 02:26:57');
+    });
+
+    test('return unix time stamp in seconds plus an additional amount of hours', () => {
+        const nowDateObject = new Date('2023-10-18T20:42:16.652Z');
+        const spy = jest.spyOn(Date, 'now').mockImplementation(() => nowDateObject.getTime());
+
+        const result = getUnixTimeStampInSeconds({ additionalHours: 24 });
+        spy.mockRestore();
+
+        expect(result).toEqual(1697748137);
+    });
+
+    test('return unix time stamp in seconds for midnight of the current day', () => {
+        const nowDateObject = new Date('2023-10-18T20:42:16.652Z');
+        const spy = jest.spyOn(Date, 'now').mockImplementation(() => nowDateObject.getTime());
+
+        const result = getUnixTimeStampInSecondsForMidnightTonight();
+        spy.mockRestore();
+
+        expect(result).toEqual(1697635800);
     });
 });
