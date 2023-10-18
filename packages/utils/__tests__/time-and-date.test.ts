8edcb5806e9058a2c335f671a2973c393050e0af
jack.mcloughlin <jackmaca@msn.com>
2023-10-18 14:49:43 +1030

    MICRO-138 set fake timers in jest to resolver pipeline error

--- packages/utils/__tests__/time-and-date.test.ts
+++ packages/utils/__tests__/time-and-date.test.ts
@@ -4,6 +4,9 @@ import {
     getUnixTimeStampInSecondsForMidnightTonight,
 } from '../time-and-date';
 
+jest.useFakeTimers();
+jest.setSystemTime(new Date('2022-10-18T20:42:16.652+00:00').getTime());
+
 describe('Time and Date tests', () => {
     test('return date converted from BC format to AC', () => {
         const result = convertDateFormat('Tue, 29 Aug 2023 02:26:57 +0000');
@@ -11,22 +14,16 @@ describe('Time and Date tests', () => {
     });
 
     test('return unix time stamp in seconds plus an additional amount of hours', () => {
-        const nowDateObject = new Date('2023-10-18T20:42:16.652+00:00');
-        const spy = jest.spyOn(Date, 'now').mockImplementation(() => nowDateObject.getTime());
-
         const result = getUnixTimeStampInSeconds({ additionalHours: 24 });
-        spy.mockRestore();
 
-        expect(result).toEqual(1697748137);
+        expect(result).toEqual(1666212137);
+        jest.useRealTimers();
     });
 
     test('return unix time stamp in seconds for midnight of the current day', () => {
-        const nowDateObject = new Date('2023-10-18T20:42:16.652+00:00');
-        const spy = jest.spyOn(Date, 'now').mockImplementation(() => nowDateObject.getTime());
-
         const result = getUnixTimeStampInSecondsForMidnightTonight();
-        spy.mockRestore();
 
-        expect(result).toEqual(1697635800);
+        expect(result).toEqual(1666186200);
+        jest.useRealTimers();
     });
 });
