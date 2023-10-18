3027e8ddeb876e58af66a5b27bee391fe3621502
Adam Hall <adam.hall@aligent.com.au>
2023-10-18 11:30:15 +1030

    Add renovate and OWASP pipelines

---
+++ renovate_config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    secrets: {
+        NPM_PUBLISH_TOKEN: process.env.NPM_PUBLISH_TOKEN,
+    },
+    $schema: 'https://docs.renovatebot.com/renovate-schema.json',
+    hostRules: [
+        {
+            matchHost: 'https://npm.corp.aligent.consulting/',
+            hostType: 'npm',
+            token: '{{ secrets.NPM_PUBLISH_TOKEN }}',
+        },
+    ],
+    npmrc: '@aligent:registry=https://npm.corp.aligent.consulting/',
+};
