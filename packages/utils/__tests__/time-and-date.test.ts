5e9f5452937f2c09cfc96534073042dc13d3c7f2
jack.mcloughlin <jackmaca@msn.com>
2023-10-23 11:21:04 +1030

    MICRO-138 added unit test for getUnixTimeStampInSecondsForMidnightTonight() that was working locally but not in pipelines

--- packages/utils/__tests__/time-and-date.test.ts
+++ packages/utils/__tests__/time-and-date.test.ts
@@ -1,8 +1,17 @@
-import { convertDateFormat } from '../time-and-date';
+import { convertDateFormat, getUnixTimeStampInSecondsForMidnightTonight } from '../time-and-date';
+
+jest.useFakeTimers();
+jest.setSystemTime(new Date('2022-10-18T20:42:16.652+00:00').getTime());
 
 describe('Time and Date tests', () => {
     test('return date conveted from BC format to AC', () => {
         const result = convertDateFormat('Tue, 29 Aug 2023 02:26:57 +0000');
         expect(result).toEqual('2023-08-29 02:26:57');
+    });
+
+    test('return unix time stamp in seconds for midnight of the current day', () => {
+        const result = getUnixTimeStampInSecondsForMidnightTonight();
+
+        expect(result).toEqual(1666186200);
     });
 });
