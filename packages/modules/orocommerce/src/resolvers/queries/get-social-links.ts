c8540ed8dd6ebf5ed62438bdd69d131a86362b70
Brett Cutting <brett.cutting@aligent.com.au>
2023-10-27 09:57:34 +1000

    OTF-101: add social link mock data to be returned in the resolver

--- packages/modules/orocommerce/src/resolvers/queries/get-social-links.ts
+++ packages/modules/orocommerce/src/resolvers/queries/get-social-links.ts
@@ -3,6 +3,17 @@ import { QueryResolvers } from '@aligent/bigcommerce-resolvers';
 export const getSocialLinksResolver: QueryResolvers['getSocialLinks'] = {
     resolve: async (_root, _args, context, _info) => {
         // @todo get and return real data
-        return [];
+        return [
+            {
+                name: 'twitter',
+                destination: 'https://twitter.com/aligent',
+                __typename: 'SocialLink',
+            },
+            {
+                name: 'linkedIn',
+                destination: 'https://www.linkedin.com/company/aligent-consulting',
+                __typename: 'SocialLink',
+            },
+        ];
     },
 };
