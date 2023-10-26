8932822b5f6c0ec5218cf847a76691fbf1859b38
Hashim.Abdullah <hashim.abdullah@MX100823.local>
2023-10-26 12:00:36 +0300

    OTF-94: remove un-used return for mock data

--- packages/modules/orocommerce/src/resolvers/queries/store-config.ts
+++ packages/modules/orocommerce/src/resolvers/queries/store-config.ts
@@ -1,13 +1,11 @@
 import { QueryResolvers } from '@aligent/orocommerce-resolvers';
 
-import { transformedStoreConfig } from '../../transformers/store-config/__tests__/__data__/store-config-transformed-data';
 import { StoreConfigApiClient } from '../../apis/rest/store-config-api-client';
 
 import { StoreConfigTransformerChain } from '../../transformers/store-config/store-config-transformer';
 
 export const storeConfigResolver: QueryResolvers['storeConfig'] = {
     resolve: async (_root, _args, context, _info) => {
-        return transformedStoreConfig;
         const storeConfigApiClient: StoreConfigApiClient =
             context.injector.get(StoreConfigApiClient);
         const oroStoreConfig = await storeConfigApiClient.getStoreConfig();
