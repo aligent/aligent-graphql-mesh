053d09620a47ca9ee7e5b4d92e9f55b952b9d9ee
Brett Cutting <brett.cutting@aligent.com.au>
2023-10-27 10:21:31 +1000

    OTF-101: add an underscore to indicate the arg isn't used

--- packages/modules/orocommerce/src/resolvers/queries/get-social-links.ts
+++ packages/modules/orocommerce/src/resolvers/queries/get-social-links.ts
@@ -1,7 +1,7 @@
 import { QueryResolvers } from '@aligent/bigcommerce-resolvers';
 
 export const getSocialLinksResolver: QueryResolvers['getSocialLinks'] = {
-    resolve: async (_root, _args, context, _info) => {
+    resolve: async (_root, _args, _context, _info) => {
         // @todo get and return real data
         return [
             {
