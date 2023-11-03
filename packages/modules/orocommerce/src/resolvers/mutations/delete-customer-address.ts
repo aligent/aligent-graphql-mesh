24104c7ac8f0e3dd833cee0fdbd967cc8a848a86
Mandeep Tatla <mandy.tatla@aligent.com.au>
2023-11-03 13:13:10 +1030

    OTF-89: Pass the destruct id instaed of assigning to the variable

--- packages/modules/orocommerce/src/resolvers/mutations/delete-customer-address.ts
+++ packages/modules/orocommerce/src/resolvers/mutations/delete-customer-address.ts
@@ -2,10 +2,9 @@ import { MutationResolvers } from '@aligent/orocommerce-resolvers';
 import { CustomerClient } from '../../apis/rest/customer';
 
 export const deleteCustomerAddressMutation: MutationResolvers['deleteCustomerAddress'] = {
-    resolve: async (_root, args, context, _info) => {
-        const addressId = args.id;
+    resolve: async (_root, { id }, context, _info) => {
         const customerClient: CustomerClient = context.injector.get(CustomerClient);
-        const response = await customerClient.deleteCustomerAddress(addressId);
+        const response = await customerClient.deleteCustomerAddress(id);
         return response;
     },
 };
