ed6203672857b0fe8517e6a03065dccecfeef207
Mandeep Tatla <mandy.tatla@aligent.com.au>
2023-11-08 17:07:16 +1030

    OTF-100; Test update

--- packages/modules/orocommerce/src/transformers/customers/__tests__/tranform-oro-customer.test.ts
+++ packages/modules/orocommerce/src/transformers/customers/__tests__/tranform-oro-customer.test.ts
@@ -4,7 +4,7 @@ import { OroAddressesTransformer } from '../transform-oro-addresses-data';
 import { oroCustomer, oroAddresses, customer } from './__data__/customer';
 
 describe('Transform  Oro customer to Takflight customer', () => {
-    test('return transformed Oro Address', () => {
+    test('return transformed Customer', () => {
         const oroAddressTransformer = new OroAddressesTransformer();
         const oroCustomerTransformer: OroCustomerTransformer = new OroCustomerTransformer(
             oroAddressTransformer
