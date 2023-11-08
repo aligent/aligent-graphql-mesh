6151f51d1b6fa9a03568c2d05d6b5d79a9744586
Bruno Pasqualini <bruno.pasqualini@aligent.com.au>
2023-11-08 13:34:28 +1030

    OTF-79: Remove _ from context parameter

--- packages/modules/orocommerce/src/resolvers/mutations/create-empty-cart.ts
+++ packages/modules/orocommerce/src/resolvers/mutations/create-empty-cart.ts
@@ -5,8 +5,8 @@ import { ShoppingListsClient } from '../../apis/rest/shopping-list-api-client';
  * If the customer has a shopping list, return it. If not create a new shopping list.
  */
 export const createEmptyCartMutation: MutationResolvers['createEmptyCart'] = {
-    resolve: async (_root, _args, _context, _info) => {
-        const client: ShoppingListsClient = _context.injector.get(ShoppingListsClient);
+    resolve: async (_root, _args, context, _info) => {
+        const client: ShoppingListsClient = context.injector.get(ShoppingListsClient);
         const shoppinglists = await client.getShoppingLists();
 
         if (shoppinglists.length === 0) {
