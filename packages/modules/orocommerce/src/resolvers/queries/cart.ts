8397258fb74adbf10eb236a1c30258e2373eda91
jack.mcloughlin <jackmaca@msn.com>
2023-11-13 20:49:56 +1030

    OTF-166 format fix

--- packages/modules/orocommerce/src/resolvers/queries/cart.ts
+++ packages/modules/orocommerce/src/resolvers/queries/cart.ts
@@ -4,8 +4,9 @@ import { CartService, ShoppingListService } from '../../services';
 export const cartResolver: QueryResolvers['cart'] = {
     resolve: async (_root, args, context, _info) => {
         const shoppingListService: ShoppingListService = context.injector.get(ShoppingListService);
-        const shoppingListWithItems =
-            await shoppingListService.getShoppingListWithItems(args.cart_id)
+        const shoppingListWithItems = await shoppingListService.getShoppingListWithItems(
+            args.cart_id
+        );
 
         const cartService: CartService = context.injector.get(CartService);
         const transformedCart = await cartService.getCart(shoppingListWithItems);
