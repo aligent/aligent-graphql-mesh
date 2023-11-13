25ec0b3376b25eee5868c7c947d1e5722d8f0a2a
jack.mcloughlin <jackmaca@msn.com>
2023-11-13 20:43:01 +1030

    OTF-116 removed un-used line for setting cart_id to default if no cart_id was sent

--- packages/modules/orocommerce/src/resolvers/queries/cart.ts
+++ packages/modules/orocommerce/src/resolvers/queries/cart.ts
@@ -2,14 +2,10 @@ import { QueryResolvers } from '@aligent/orocommerce-resolvers';
 import { CartService, ShoppingListService } from '../../services';
 
 export const cartResolver: QueryResolvers['cart'] = {
-    resolve: async (_root, { cart_id }, context, _info) => {
-        // If no ID is sent then ORO will attempt to add the default shopping list
-        // If no list exists a new one will be created
-        const shoppingListId = cart_id === '' ? 'default' : cart_id;
-
+    resolve: async (_root, args, context, _info) => {
         const shoppingListService: ShoppingListService = context.injector.get(ShoppingListService);
         const shoppingListWithItems =
-            await shoppingListService.getShoppingListWithItems(shoppingListId);
+            await shoppingListService.getShoppingListWithItems(args.cart_id)
 
         const cartService: CartService = context.injector.get(CartService);
         const transformedCart = await cartService.getCart(shoppingListWithItems);
