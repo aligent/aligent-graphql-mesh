8404ee75ace310e2799368ca1004793e9f3149a6
jack.mcloughlin <jackmaca@msn.com>
2023-11-14 10:52:38 +1030

    OTF-116 refactored getCart service to also accept a string for the shopping list ID

--- packages/modules/orocommerce/src/resolvers/queries/cart.ts
+++ packages/modules/orocommerce/src/resolvers/queries/cart.ts
@@ -1,15 +1,10 @@
 import { QueryResolvers } from '@aligent/orocommerce-resolvers';
-import { CartService, ShoppingListService } from '../../services';
+import { CartService } from '../../services';
 
 export const cartResolver: QueryResolvers['cart'] = {
     resolve: async (_root, args, context, _info) => {
-        const shoppingListService: ShoppingListService = context.injector.get(ShoppingListService);
-        const shoppingListWithItems = await shoppingListService.getShoppingListWithItems(
-            args.cart_id
-        );
-
         const cartService: CartService = context.injector.get(CartService);
-        const transformedCart = await cartService.getCart(shoppingListWithItems);
+        const transformedCart = cartService.getCart(args.cart_id);
 
         return transformedCart;
     },
