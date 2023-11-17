f5c4be72985d521963a4d8a1562c3e40be364ffe
Brett Cutting <brett.cutting@aligent.com.au>
2023-11-17 11:25:27 +1000

    OTF-125: update the createCartRedirectUrlsResolver to call the checkoutService.getCheckout method to initialize a checkout instance for a shopping list rather than doing the login in the resolver

--- packages/modules/orocommerce/src/resolvers/mutations/create-cart-redirect-urls.ts
+++ packages/modules/orocommerce/src/resolvers/mutations/create-cart-redirect-urls.ts
@@ -1,7 +1,6 @@
-import { GraphQLError } from 'graphql';
 import { MutationResolvers } from '@aligent/orocommerce-resolvers';
-import { CheckoutSourcesClient } from '../../apis/rest';
-import { getActiveCheckoutSourcesFromShoppingListId } from '../../utils';
+import { CheckoutService } from '@aligent/orocommerce-graphql-module';
+import { AxiosError } from 'axios';
 
 const SUCCESS_RESPONSE = {
     data: {
@@ -18,46 +17,29 @@ const getCreateCheckoutSourceError = (shoppingListId: number) => {
 export const createCartRedirectUrlsResolver: MutationResolvers['createCartRedirectUrls'] = {
     resolve: async (_root, args, context, _info) => {
         const shoppingListId = Number(args.cartId);
+
         if (isNaN(shoppingListId)) {
-            throw new GraphQLError('The "cartId" input is not a number');
-        }
-        const checkoutSourcesClient: CheckoutSourcesClient =
-            context.injector.get(CheckoutSourcesClient);
-
-        /* @todo BE OTF-126 need to provide option to filter out orders which are soft deleted. This is shown
-         *  by the "deleted: true" property on a checkout source object  */
-        const checkoutSources = await checkoutSourcesClient.getCheckoutSources({
-            'filter[shoppingList]': shoppingListId,
-            sort: '-id', // Order from the most recently created shopping list id's
-        });
-
-        /* A shopping list can have multiple checkout sources so lets find the active ones */
-        const matchingCheckoutSources = getActiveCheckoutSourcesFromShoppingListId(
-            checkoutSources,
-            shoppingListId
-        );
-
-        /* If an active checkout source exists we're safe to tell the PWA it can redirect to the checkout
-         * by returning the checkout_url  */
-        if (matchingCheckoutSources.length > 0) {
-            return SUCCESS_RESPONSE;
+            throw new Error('The "cartId" input string should only contain numbers');
         }
 
-        /* If no checkout source can be found, create one for the shopping list id passed in*/
+        const checkoutService: CheckoutService = context.injector.get(CheckoutService);
+
         try {
-            const createdCheckoutSource = await checkoutSourcesClient.createCheckoutSource({
-                shoppingList: shoppingListId,
-            });
+            const checkout = await checkoutService.getCheckout(shoppingListId);
 
-            if (!createdCheckoutSource?.id) {
-                throw new GraphQLError(getCreateCheckoutSourceError(shoppingListId));
+            if (!checkout?.id) {
+                throw new Error(getCreateCheckoutSourceError(shoppingListId));
             }
 
             /* Once a checkout source is successfully created, tell the PWA it can redirect to the checkout
              * by returning the checkout_url  */
             return SUCCESS_RESPONSE;
         } catch (e) {
-            throw new GraphQLError(getCreateCheckoutSourceError(shoppingListId));
+            if (e instanceof AxiosError && e.response?.data?.[0]?.detail) {
+                throw new Error(e.response.data[0].detail);
+            }
+
+            throw new Error(getCreateCheckoutSourceError(shoppingListId));
         }
     },
 };
