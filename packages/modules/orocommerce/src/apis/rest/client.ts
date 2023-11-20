384f8e71fcf932039797dda2e3dadc9141b870ce
Bruno Pasqualini <bruno.pasqualini@aligent.com.au>
2023-11-20 16:09:51 +1030

    OTF-131: Add axios error handling in client class

--- packages/modules/orocommerce/src/apis/rest/client.ts
+++ packages/modules/orocommerce/src/apis/rest/client.ts
@@ -1,9 +1,8 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, HttpStatusCode } from 'axios';
 import { Inject, Injectable, forwardRef } from 'graphql-modules';
 import { StoreUrl } from '../../providers';
-import { Auth } from '../../services';
-import { MetaAllowedTypes, ProductSearchMeta } from '../../types';
-
+import { Auth } from '../../services/auth';
+import { GraphqlError } from '@aligent/utils';
 // @TOOO: Set version based on NPM package version
 export const USER_AGENT = 'AligentMesh / 0.0.1';
 const API_PATH = '/api';
@@ -30,36 +29,44 @@ export class ApiClient {
             (response) => response,
             (error) => {
                 if (axios.isAxiosError(error)) {
-                    console.log(error.message);
-                    if (error.response) {
+                    const response = error.response;
+                    if (response) {
                         console.error(error.toJSON());
+                    }
+                    const graphqlErrorMessage = JSON.stringify({
+                        statusCode: response?.status,
+                        statusText: response?.statusText,
+                        // Oro might return a response body even though it isn't a 200
+                        // https://jsonapi.org/format/#errors-processing
+                        data: response?.data,
+                    });
+                    const httpStatus = response?.status;
+                    switch (httpStatus) {
+                        // 400 errors - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses
+                        case HttpStatusCode.BadRequest:
+                            throw new GraphqlError('input', graphqlErrorMessage);
+                        case HttpStatusCode.Unauthorized:
+                        case HttpStatusCode.Forbidden:
+                            throw new GraphqlError('authorization', graphqlErrorMessage);
+                        case HttpStatusCode.NotFound:
+                            throw new GraphqlError('no-such-entity', graphqlErrorMessage);
+                        case HttpStatusCode.Conflict:
+                            throw new GraphqlError('already-exists', graphqlErrorMessage);
+                        case HttpStatusCode.InternalServerError:
+                            throw new GraphqlError('server-internal-error', graphqlErrorMessage);
+                        default:
+                            throw new Error(graphqlErrorMessage);
                     }
                 } else {
                     console.error(error);
                 }
-
                 return Promise.reject(error);
             }
         );
     }
 
     async get<T, D = undefined>(url: string, config?: AxiosRequestConfig) {
-        const response = await this.client.get<{ data: T; included?: D; meta?: MetaAllowedTypes }>(
-            url,
-            config
-        );
-        // In Oro, by providing additional requests header parameters,
-        // it is possible to retrieve additional information, such as the total number of records .
-        // The X-Include request header can be used for such purposes.
-        // https://doc.oroinc.com/api/http-header-specifics/#web-services-api-http-header-specifics
-        // Move total count from response header to meta section of the response object for convenience
-        if (!isNaN(response.headers['x-include-total-count'])) {
-            if (response.data.meta === undefined) response.data.meta = {} as ProductSearchMeta;
-            response.data.meta.totalRecordsCount = Number(
-                response.headers['x-include-total-count']
-            );
-        }
-
+        const response = await this.client.get<{ data: T; included?: D }>(url, config);
         return response.data;
     }
 
