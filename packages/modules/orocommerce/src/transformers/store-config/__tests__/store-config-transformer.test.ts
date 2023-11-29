9b876439fa92a5b6bb38a7b9487429e5f53c797f
jack.mcloughlin <jackmaca@msn.com>
2023-11-29 16:16:32 +1030

    OTF-94 replaced data is returned from custom store config API in resolver

--- packages/modules/orocommerce/src/transformers/store-config/__tests__/store-config-transformer.test.ts
+++ packages/modules/orocommerce/src/transformers/store-config/__tests__/store-config-transformer.test.ts
@@ -2,12 +2,12 @@ import 'reflect-metadata';
 
 import { StoreConfigTransformer } from '../store-config-transformer';
 import { transformedStoreConfig } from './__data__/store-config-transformed-data';
-import { getMockOroStoreConfig } from './__data__/oro-store-config-data';
+import { storeConfigData } from './__data__/oro-store-config-data';
 
 describe('Store Config data transform tests', () => {
     test('return transformed Store Config', () => {
         const storeConfigTransformer: StoreConfigTransformer = new StoreConfigTransformer();
-        const transformed = storeConfigTransformer.transform({ data: getMockOroStoreConfig() });
+        const transformed = storeConfigTransformer.transform({ data: storeConfigData });
         expect(transformed).toEqual(transformedStoreConfig);
     });
 });
