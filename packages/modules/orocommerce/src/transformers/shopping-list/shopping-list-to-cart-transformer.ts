bab3c1292b32a37cd9f72f9c8c3ff2f057bd1a4e
jack.mcloughlin <jackmaca@msn.com>
2023-11-29 14:21:58 +1030

    OTF-215 added logic to get shoppinglist without ID and then transform

--- packages/modules/orocommerce/src/transformers/shopping-list/shopping-list-to-cart-transformer.ts
+++ packages/modules/orocommerce/src/transformers/shopping-list/shopping-list-to-cart-transformer.ts
@@ -13,17 +13,7 @@ import {
     isProductImage,
     isProductCategory,
 } from '../../utils/type-predicates';
-
-const UNDEFINED_CART: Cart = {
-    id: '',
-    items: [],
-    total_quantity: 0,
-    available_gift_wrappings: [],
-    gift_receipt_included: false,
-    is_virtual: false,
-    printed_card_included: false,
-    shipping_addresses: [],
-};
+import { UNDEFINED_CART } from './constants';
 
 @Injectable()
 export class ShoppingListToCartTransformer implements Transformer<ShoppingListWithItems, Cart> {
