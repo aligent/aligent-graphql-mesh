a598d3bfa85ef796c2f105d64643cdc834d04e5b
Greg Ziborov <greg.ziborov@aligent.com.au>
2023-12-12 15:01:51 +1030

    OTF-132: update of multiple cart items

--- packages/modules/orocommerce/src/resolvers/mutations/update-cart-items.ts
+++ packages/modules/orocommerce/src/resolvers/mutations/update-cart-items.ts
@@ -7,33 +7,36 @@ import { atob } from '@aligent/utils';
 export const updateCartItemsMutation: MutationResolvers['updateCartItems'] = {
     resolve: async (_root, args, context, _info): Promise<UpdateCartItemsOutput> => {
         const { cart_id, cart_items } = args.input || {};
-        // OTF-132:Enhancement feature to handle updating multiple items for a cart in one API call.
 
-        if (!cart_id || !cart_items?.[0]?.cart_item_uid) {
-            throw new Error(`Missing update cart information`);
-        }
-
-        const { quantity } = cart_items[0];
-
-        if (quantity === 0) {
-            throw new Error(`Quantity cannot be "0"`);
-        }
-
-        if (!quantity || isNaN(quantity)) {
-            throw new Error(`Cart item quantity is incorrect`);
-        }
-
-        const cartItemId = atob(cart_items?.[0]?.cart_item_uid);
-
+        const shoppingListClient: ShoppingListsClient = context.injector.get(ShoppingListsClient);
         const updateCartItemTransformer: UpdateCartItemTransformerChain = context.injector.get(
             UpdateCartItemTransformerChain
         );
-        const transformedCartItemInput = updateCartItemTransformer.transform({
-            data: { cartItemId, quantity },
-        });
 
-        const shoppingListClient: ShoppingListsClient = context.injector.get(ShoppingListsClient);
-        await shoppingListClient.updateItemInShoppingList(transformedCartItemInput);
+        // OTF-132:Enhancement feature to handle updating multiple items for a cart in one batch update API call.
+        for (const item of cart_items) {
+            const { quantity, cart_item_uid } = item;
+
+            if (!cart_id || !cart_item_uid) {
+                throw new Error(`Missing update cart information`);
+            }
+
+            if (quantity === 0) {
+                throw new Error(`Quantity cannot be "0"`);
+            }
+
+            if (!quantity || isNaN(quantity)) {
+                throw new Error(`Cart item quantity is incorrect`);
+            }
+
+            const cartItemId = atob(cart_item_uid);
+
+            const transformedCartItemInput = updateCartItemTransformer.transform({
+                data: { cartItemId, quantity },
+            });
+
+            await shoppingListClient.updateItemInShoppingList(transformedCartItemInput);
+        }
 
         const cartService: CartService = context.injector.get(CartService);
         const transformedCart = await cartService.getCart(cart_id);
