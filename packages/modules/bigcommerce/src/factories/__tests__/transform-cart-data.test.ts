12e6fcf6ff7a529225965ba366774ddd4ede5ab8
Alan Chen <alan.chen@alig2219.staff.aligent.consulting>
2024-01-05 09:56:04 +1030

    AM-202: modify test data value

--- packages/modules/bigcommerce/src/factories/__tests__/transform-cart-data.test.ts
+++ packages/modules/bigcommerce/src/factories/__tests__/transform-cart-data.test.ts
@@ -46,7 +46,7 @@ describe('transform-cart-data', () => {
             error_type: null,
             email: undefined,
             available_payment_methods: undefined,
-            selected_payment_method: undefined,
+            selected_payment_method: null,
             items: [],
             is_virtual: false,
             free_shipping_details: {
@@ -97,7 +97,7 @@ describe('transform-cart-data', () => {
             error_type: null,
             email: undefined,
             available_payment_methods: undefined,
-            selected_payment_method: undefined,
+            selected_payment_method: null,
             free_shipping_details: {
                 free_shipping_active: false,
                 free_shipping_percentage: 100,
