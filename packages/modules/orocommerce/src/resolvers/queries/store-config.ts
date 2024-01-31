073507fdcf3dd10e141551156c9e0254a133a09c
Greg Ziborov <greg.ziborov@aligent.com.au>
2024-01-31 15:23:04 +1030

    PAMA-208: store and retreive store configs from cache

--- packages/modules/orocommerce/src/resolvers/queries/store-config.ts
+++ packages/modules/orocommerce/src/resolvers/queries/store-config.ts
@@ -1,24 +1,12 @@
 import { QueryResolvers } from '@aligent/orocommerce-resolvers';
-
 import { StoreConfigApiClient } from '../../apis/rest/store-config-api-client';
-
 import { StoreConfigTransformerChain } from '../../transformers/store-config/store-config-transformer';
-import { Auth } from '../../services';
 
 export const storeConfigResolver: QueryResolvers['storeConfig'] = {
     resolve: async (_root, _args, context, _info) => {
-        let bearerToken = context.request.headers.get('authorization');
-
-        // This is only needed when the PWA builds
-        if (!bearerToken) {
-            const authService: Auth = context.injector.get(Auth);
-            const { access_token } = await authService.login('guest', 'guest');
-            bearerToken = `Bearer ${access_token}`;
-        }
-
         const storeConfigApiClient: StoreConfigApiClient =
             context.injector.get(StoreConfigApiClient);
-        const oroStoreConfig = await storeConfigApiClient.getStoreConfig(bearerToken);
+        const oroStoreConfig = await storeConfigApiClient.retrieveStoreConfigFromCache();
 
         const storeConfigTransformerChain: StoreConfigTransformerChain = context.injector.get(
             StoreConfigTransformerChain
