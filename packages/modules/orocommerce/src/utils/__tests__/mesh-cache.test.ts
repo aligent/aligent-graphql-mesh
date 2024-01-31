861473c8c4fbb295b68a062084b28eb275bff3a7
Greg Ziborov <greg.ziborov@aligent.com.au>
2024-01-31 15:22:15 +1030

    PAMA-208: Add cache utility and unit tests

---
+++ packages/modules/orocommerce/src/utils/__tests__/mesh-cache.test.ts
@@ -0,0 +1,72 @@
+import { getDataFromMeshCache, TTL_IN_SECONDS } from '../mesh-cache';
+
+const cache: {
+    [key: string]: string;
+} = {
+    data: 'cached data',
+};
+
+const getCache = jest.fn().mockImplementation(async (cacheKey: string): Promise<unknown> => {
+    return cache[cacheKey];
+});
+
+const setCache = jest.fn().mockImplementation(async (cacheKey, response): Promise<unknown> => {
+    return (cache[cacheKey] = response);
+});
+
+const mockContext = {
+    cache: {
+        get: getCache,
+        set: setCache,
+    },
+};
+
+/* This is the query to be performed if no cached data exists*/
+const query = new Promise((resolve) => {
+    resolve('fresh data');
+});
+
+describe('mesh-cache', () => {
+    afterEach(() => {
+        setCache.mockClear();
+    });
+
+    it(`Can retrieve data from the cache`, async () => {
+        const dataFromCache = await getDataFromMeshCache(
+            mockContext as unknown as GraphQLModules.ModuleContext,
+            'data',
+            query
+        );
+
+        const expectedResponse = cache.data;
+
+        expect(dataFromCache).toBe(expectedResponse);
+        expect(getCache).toBeCalledTimes(1);
+    });
+
+    it(`can get fresh data when cached data doesn't exist`, async () => {
+        const dataFromCache = await getDataFromMeshCache(
+            mockContext as unknown as GraphQLModules.ModuleContext,
+            'no_cached_data_key',
+            query
+        );
+
+        const expectedResponse = 'fresh data';
+
+        expect(dataFromCache).toBe(expectedResponse);
+        expect(setCache).toBeCalledWith('no_cached_data_key', 'fresh data', TTL_IN_SECONDS);
+    });
+
+    it(`Does not attempt to set new query data if there's no cache key`, async () => {
+        const dataFromCache = await getDataFromMeshCache(
+            mockContext as unknown as GraphQLModules.ModuleContext,
+            '',
+            query
+        );
+
+        const expectedResponse = 'fresh data';
+
+        expect(dataFromCache).toBe(expectedResponse);
+        expect(setCache).not.toBeCalled();
+    });
+});
---
+++ packages/modules/orocommerce/src/utils/mesh-cache.ts
@@ -0,0 +1,32 @@
+import { AxiosResponse } from 'axios';
+
+// The time we want the cached data to live. 30 minutes
+export const TTL_IN_SECONDS: number = 1800;
+
+/**
+ * Searches for data in the mesh context cache, otherwise makes a request for
+ * the wanted data and stores the response in the cache.
+ *
+ * @param context
+ * @param cacheKey
+ * @param query
+ */
+export const getDataFromMeshCache = async (
+    context: GraphQLModules.ModuleContext,
+    cacheKey: string,
+    query: unknown
+): Promise<AxiosResponse['data']> => {
+    let response = await context.cache.get(cacheKey);
+    console.log('CACHE: ', context.cache);
+    //await context.cache.delete(cacheKey);
+
+    if (!response && query) {
+        response = await query;
+
+        if (!cacheKey) return response;
+        await context.cache.set(cacheKey, response, TTL_IN_SECONDS);
+        console.log('CACHE SET: ', context.cache);
+    }
+
+    return response;
+};
