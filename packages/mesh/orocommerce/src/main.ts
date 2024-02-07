91e76d92d42245176318672f579fb72cb0be415b
jack.mcloughlin <jackmaca@msn.com>
2024-02-07 14:11:59 +1030

    OTF-277 pulled requisition list out of customer resolve to create sub resolver

--- packages/mesh/orocommerce/src/main.ts
+++ packages/mesh/orocommerce/src/main.ts
@@ -34,7 +34,7 @@ const yoga = createYoga({
                 n: 15000,
             },
             costLimit: {
-                maxCost: 15000,
+                maxCost: 50000,
             },
         }),
     ],
