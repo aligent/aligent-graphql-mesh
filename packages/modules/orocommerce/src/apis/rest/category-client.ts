fe76b858c70d924b88bbb9a24a1a683f25baec92
Greg Ziborov <greg.ziborov@aligent.com.au>
2024-02-07 11:44:04 +1030

    PAMA-210: Update category api clients and resolvers to use correct catalog id to fetch oro data depending on api used

--- packages/modules/orocommerce/src/apis/rest/category-client.ts
+++ packages/modules/orocommerce/src/apis/rest/category-client.ts
@@ -1,32 +1,30 @@
 import { forwardRef, Inject, Injectable } from 'graphql-modules';
 import { ApiClient } from './client';
-import {
-    WebCatalogTree as OroWebCatalogCategory,
-    Category as OroProductCategory,
-} from '../../types';
+import { WebCatalogTree as OroCategory } from '../../types';
 
 @Injectable()
 export class CategoriesClient {
     constructor(@Inject(forwardRef(() => ApiClient)) protected apiClient: ApiClient) {}
 
-    async getCategories(rootEntityId: number | null): Promise<OroWebCatalogCategory[]> {
+    async getCategories(rootEntityId: number | null): Promise<OroCategory[]> {
         const path = `/webcatalogtree`;
         const params = {
             'filter[root][gte]': rootEntityId,
         };
-        const response = await this.apiClient.get<OroWebCatalogCategory[]>(path, { params });
+        const response = await this.apiClient.get<OroCategory[]>(path, { params });
         return response.data;
     }
 
-    async getBreadcrumbsFromWebCatalog(nodeEntityId: number): Promise<OroWebCatalogCategory[]> {
-        const path = `/webcatalogtree/${nodeEntityId}/path`;
-        const response = await this.apiClient.get<OroWebCatalogCategory[]>(path);
-        return response.data;
-    }
-
-    async getBreadcrumbsFromMasterCatalog(nodeEntityId: number): Promise<OroProductCategory[]> {
-        const path = `/mastercatalogcategories/${nodeEntityId}/categoryPath`;
-        const response = await this.apiClient.get<OroProductCategory[]>(path);
-        return response.data;
+    async getBreadcrumbs(nodeEntityId: number): Promise<OroCategory[]> {
+        const path = `/webcatalogtree/${nodeEntityId}`;
+        const params = {
+            include: 'path',
+        };
+        // 'path' returns category tree from home to the parent of 'nodeEntityId' category
+        // we need to merge current category to get complete breadcrumbs
+        const response = await this.apiClient.get<OroCategory, OroCategory[]>(path, { params });
+        if (response.included && response.included.length > 0)
+            return [...response.included, response.data];
+        return [response.data];
     }
 }
