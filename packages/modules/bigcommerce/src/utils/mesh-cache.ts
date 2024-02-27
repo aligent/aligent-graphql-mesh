9356b048c662b521fc1db02fe271571f7ef1421a
Brett Cutting <brett.cutting@aligent.com.au>
2024-02-28 08:22:41 +1000

    AM-442: Update the doc block comment for the getDataFromMeshCache function

--- packages/modules/bigcommerce/src/utils/mesh-cache.ts
+++ packages/modules/bigcommerce/src/utils/mesh-cache.ts
@@ -7,14 +7,21 @@ export const TTL_IN_MILLI_SECONDS = 1800000;
  * Searches for data in the mesh context cache, otherwise makes a request for
  * the wanted data and stores the response in the cache.
  *
- * @param context
- * @param cacheKey
- * @param query
+ * @param context - The Graphql module context instance.
+ * @param cacheKey - The key which the query response should be cached under in the context.cache
+ * @param query - The query to be invoked should the corresponding response not be stored
+ *                in the cache. This is required to an arrow function, so it can hold the
+ *                arguments without being invoked. e.g. "() => myFunction(arg1: "arg", arg2: true)"
+ *
+ * usage:
+ * const uninvokedQuery = () => getStoreConfigs(context);
+ * const dataFromCache = await getDataFromMeshCache(context, "store-config", uninvokedQuery);
+ *
  */
 export const getDataFromMeshCache = async (
     context: GraphQLModules.ModuleContext,
     cacheKey: string,
-    query: () => unknown
+    query: () => Promise<unknown>
 ): Promise<AxiosResponse['data']> => {
     let response = await context.cache.get(cacheKey);
 
