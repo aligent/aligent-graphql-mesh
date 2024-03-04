791e1feaaa3bf3280568d8964d101e483cb6e9cc
Alan Chen <alan.chen@alig2219.staff.aligent.consulting>
2024-03-04 10:38:55 +1030

    AM-265: change array comparing logic

--- packages/modules/bigcommerce/src/utils/compare-arrays.ts
+++ packages/modules/bigcommerce/src/utils/compare-arrays.ts
@@ -3,25 +3,14 @@ export const compareArraysRegardlessElementOrder = (
     inputArray?: Array<string[]>
 ) => {
     if (!inputArray) return false;
-    // Convert the input array to a set for easy comparison
-    const inputArraySet = new Set(inputArray[inputArray.length - 1]);
 
-    // Check if the input array contains the elements of any target array
-    for (const targetElement of targetArray) {
-        const targetSet = new Set(targetElement);
-        let isEqual = true;
+    const sortedTargetArrayString = sortNestedArray(targetArray);
+    const sortedInputArrayString = sortNestedArray(inputArray);
 
-        for (const element of targetSet) {
-            if (!inputArraySet.has(element)) {
-                isEqual = false;
-                break;
-            }
-        }
+    return sortedTargetArrayString === sortedInputArrayString;
+};
 
-        if (isEqual) {
-            return true;
-        }
-    }
-
-    return false;
+// Convert nested array eg. [[ '131', '149' ], [ '132', '152' ]] to "131132149152" for easy comparison
+const sortNestedArray = (array: Array<string[] | undefined>): string => {
+    return array.toString().split(',').sort().join('');
 };
