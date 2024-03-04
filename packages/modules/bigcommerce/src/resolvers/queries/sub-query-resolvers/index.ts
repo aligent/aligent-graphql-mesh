96f78016edd62109226cdb16c6fd3801d45e0f48
Brett Cutting <brett.cutting@aligent.com.au>
2024-03-04 15:20:01 +1000

    AM-551: move all the sub-resolvers into an index file Add customerIsSubscribedResolver and customerWishlistsResolver to the Customer resolvers

---
+++ packages/modules/bigcommerce/src/resolvers/queries/sub-query-resolvers/index.ts
@@ -0,0 +1,7 @@
+export * from './customer-is-subscribed';
+export * from './customerOrderItems';
+export * from './customerOrders';
+export * from './customerOrderShippingAddress';
+export * from './customerOrderShippingMethod';
+export * from './customer-total';
+export * from './customer-wishlists';
