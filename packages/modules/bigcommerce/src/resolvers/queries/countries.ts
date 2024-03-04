a41e586c00baad491fa4a124b05f98b08fcc1b84
Brett Cutting <brett.cutting@aligent.com.au>
2024-03-04 18:34:45 +1000

    AM-551: return the transformed countries data response from  retrieveCountriesFromCache update enriched-cart to pass the transformed countries data directly to getTransformedCartData move queries into the Promise.all to tidy things up

--- packages/modules/bigcommerce/src/resolvers/queries/countries.ts
+++ packages/modules/bigcommerce/src/resolvers/queries/countries.ts
@@ -1,22 +1,23 @@
-import { QueryResolvers } from '@aligent/bigcommerce-resolvers';
+import { Country, QueryResolvers } from '@aligent/bigcommerce-resolvers';
 import { getAllStates, getCountries } from '../../apis/rest/countries';
 import { transformCountriesAndStates } from '../../factories/transform-countries-data';
 import { getDataFromMeshCache } from '../../utils/mesh-cache';
 import { CACHE_KEY__COUNTRIES } from '../../constants';
 
-/* istanbul ignore next */
 export const countriesResolver: QueryResolvers['countries'] = {
     resolve: async (_root, _args, context, _info) => {
-        const [countries, states] = await retrieveCountriesAndStatesFromCache(context);
-
-        return transformCountriesAndStates(countries, states);
+        return retrieveCountriesFromCache(context);
     },
 };
 
-export const retrieveCountriesAndStatesFromCache = async (
+export const retrieveCountriesFromCache = async (
     context: GraphQLModules.ModuleContext
-) => {
-    const query = () => Promise.all([getCountries(), getAllStates(1)]);
+): Promise<Country[]> => {
+    const query = async () => {
+        const [countries, states] = await Promise.all([getCountries(), getAllStates(1)]);
+
+        return transformCountriesAndStates(countries, states);
+    };
 
     return getDataFromMeshCache(context, CACHE_KEY__COUNTRIES, query);
 };
