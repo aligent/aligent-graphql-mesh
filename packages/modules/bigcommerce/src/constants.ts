d23af7bb3eb095a64e134ee0255e18ef3e1f3cfc
Brett Cutting <brett.cutting@aligent.com.au>
2024-03-06 13:06:04 +1000

    AM-546: add the missing CACHE_KEY__CUSTOMER_IMPERSONATION_TOKEN const and add mesh-cache to index.ts

--- packages/modules/bigcommerce/src/constants.ts
+++ packages/modules/bigcommerce/src/constants.ts
@@ -2,6 +2,7 @@ export const CACHE_KEY__BRANDS = 'brands';
 export const CACHE_KEY__CATEGORIES = 'categories';
 export const CACHE_KEY__COUNTRIES = 'countries';
 export const CACHE_KEY__CUSTOMER_ATTRIBUTES = 'customer_attributes';
+export const CACHE_KEY__CUSTOMER_IMPERSONATION_TOKEN = 'customer_impersonation_token';
 export const CACHE_KEY__STOREFRONT_FORM_FIELDS = 'storefront_form_fields';
 export const CACHE_KEY__STORE_CONFIG = 'store_configs';
 
@@ -10,6 +11,7 @@ export type CacheItemTtlTypes = {
     [CACHE_KEY__CATEGORIES]: number;
     [CACHE_KEY__COUNTRIES]: number;
     [CACHE_KEY__CUSTOMER_ATTRIBUTES]: number;
+    [CACHE_KEY__CUSTOMER_IMPERSONATION_TOKEN]: number;
     [CACHE_KEY__STOREFRONT_FORM_FIELDS]: number;
     [CACHE_KEY__STORE_CONFIG]: number;
     [key: string]: number;
@@ -23,6 +25,7 @@ export const CACHE_ITEMS_TTL: CacheItemTtlTypes = {
     [CACHE_KEY__CATEGORIES]: 600000, // 10 minutes
     [CACHE_KEY__COUNTRIES]: 1800000, // 30 minutes
     [CACHE_KEY__CUSTOMER_ATTRIBUTES]: 1800000, // 30 minutes
+    [CACHE_KEY__CUSTOMER_IMPERSONATION_TOKEN]: 86400000, // 24 hours
     [CACHE_KEY__STOREFRONT_FORM_FIELDS]: 1800000, // 30 minutes
     [CACHE_KEY__STORE_CONFIG]: 1800000, // 30 minutes
 };
