f243397a9cb1678387fa27f0c9217db366513787
Adam Hall <adam.hall@aligent.com.au>
2024-03-07 09:42:44 +1030

    Add skeleton for graphql module generator

---
+++ tools/plugin/aligent-mesh/src/generators/graphql-module/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nx/devkit';
+
+import { graphqlModuleGenerator } from './generator';
+import { GraphqlModuleGeneratorSchema } from './schema';
+
+describe('graphql-module generator', () => {
+    let tree: Tree;
+    const options: GraphqlModuleGeneratorSchema = { name: 'test' };
+
+    beforeEach(() => {
+        tree = createTreeWithEmptyWorkspace();
+    });
+
+    it('should run successfully', async () => {
+        await graphqlModuleGenerator(tree, options);
+        const config = readProjectConfiguration(tree, 'test');
+        expect(config).toBeDefined();
+    });
+});
