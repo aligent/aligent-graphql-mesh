4838ee0607d3421f31e3ad88852d2517ffc5fce9
Brett Cutting <brett.cutting@aligent.com.au>
2024-03-18 12:32:06 +1000

    AM-596: add refreshCustomerToken to resolvers remove access_token from refreshCustomerToken mutation args as we can get this from headers

--- packages/modules/auth/src/resolvers/index.ts
+++ packages/modules/auth/src/resolvers/index.ts
@@ -1,9 +1,11 @@
 import { Resolvers } from '@aligent/auth-resolvers';
 import { generateCustomerTokenResolver } from './mutations/generate-customer-token';
+import { refreshCustomerTokenResolver } from './mutations/refresh-customer-token';
 
 export const resolvers: Resolvers = {
     Mutation: {
         generateCustomerToken: generateCustomerTokenResolver,
+        refreshCustomerToken: refreshCustomerTokenResolver,
     },
 };
 
