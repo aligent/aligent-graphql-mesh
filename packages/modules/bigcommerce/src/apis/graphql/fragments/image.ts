9b7accf2162a200fd1e0bd54256859a4eb684bf3
Gowri Sankar <gowri.sankar@aligent.com.au>
2024-05-22 13:12:20 +0930

    SL-238: add url template to product image field

--- packages/modules/bigcommerce/src/apis/graphql/fragments/image.ts
+++ packages/modules/bigcommerce/src/apis/graphql/fragments/image.ts
@@ -3,6 +3,7 @@ import { gql } from 'graphql-tag';
 export const image = gql`
     fragment Image on Image {
         url(width: 500, height: 245)
+        urlTemplate
         urlOriginal
         altText
         isDefault
