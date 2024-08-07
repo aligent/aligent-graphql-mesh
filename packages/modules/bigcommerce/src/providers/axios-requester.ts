ebdb6c09fff3861280e50d8cdab26d45d8c9776d
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2024-08-07 11:41:49 +0930

    MI-3: Further abstracted axios-based SDK so that provider code does not import any axios references.
    Also installed type-fest, used it to create some utility types to ease working with the SDK in typescript, and added doc blocks for provider code

--- packages/modules/bigcommerce/src/providers/axios-requester.ts
+++ packages/modules/bigcommerce/src/providers/axios-requester.ts
@@ -1,15 +1,37 @@
 import { DocumentNode } from 'graphql';
 import * as xray from 'aws-xray-sdk';
-
-import { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { print } from 'graphql';
 
-const validDocDefOps = ['mutation', 'query', 'subscription'];
+type ErrorHandler = (error: unknown, label: string) => never;
 
-export function getAxiosRequester(
-    client: AxiosInstance,
-    onError: (error: unknown, label: string) => never
-) {
+// Only including mutations and queries as we have not
+// tested subscriptions and can't guarantee support for them
+const validDocumentTypes = ['mutation', 'query'];
+
+/**
+ * Factory to generate requester for passing to codegen getSDK functions
+ *
+ * This abstracts the axios specific implementation details, requiring
+ * consumers to pass in only generic configuration options. It also allows
+ * the consumer to choose a different getSDK function to the one packaged with
+ * this module.
+ */
+export const requesterFactory = (config: {
+    graphqlEndpoint: string;
+    timeout: { seconds: number; message: string };
+    onError: ErrorHandler;
+}) => {
+    const client = axios.create({
+        baseURL: config.graphqlEndpoint,
+        headers: {
+            accept: 'application/json',
+        },
+        timeout: config.timeout.seconds,
+        timeoutErrorMessage: config.timeout.message,
+        signal: AbortSignal.timeout(config.timeout.seconds),
+    });
+
     const requester = async <R, V>(
         doc: DocumentNode,
         variables: V,
@@ -18,7 +40,7 @@ export function getAxiosRequester(
         // Valid document should contain *single* query or mutation unless it's has a fragment
         if (
             doc.definitions.filter(
-                (d) => d.kind === 'OperationDefinition' && validDocDefOps.includes(d.operation)
+                (d) => d.kind === 'OperationDefinition' && validDocumentTypes.includes(d.operation)
             ).length !== 1
         ) {
             throw new Error('DocumentNode must contain a single query or mutation');
@@ -47,9 +69,9 @@ export function getAxiosRequester(
 
             return response.data;
         } catch (error: unknown) {
-            return onError(error, getAxiosRequester.name);
+            return config.onError(error, requesterFactory.name);
         }
     };
 
     return requester;
-}
+};
--- packages/modules/bigcommerce/src/providers/index.ts
+++ packages/modules/bigcommerce/src/providers/index.ts
@@ -1,15 +1,76 @@
 import { InjectionToken, Provider, Scope } from 'graphql-modules';
 import { BigCommerceModuleConfig } from '../index';
-import { getSdk, Sdk } from '@aligent/bigcommerce-operations';
-import axios from 'axios';
-import { getAxiosRequester } from './axios-requester';
+import { getSdk } from '@aligent/bigcommerce-operations';
+import { requesterFactory } from './axios-requester';
 import { logAndThrowError } from '@aligent/utils';
+import { Get, Paths } from 'type-fest';
 
 export const ModuleConfig = new InjectionToken<BigCommerceModuleConfig>(
     'Configuration for the BigCommerce GraphQL Module'
 );
-export const BigCommerceSdk = new InjectionToken<Sdk>('');
 
+/**
+ * Generates a BigCommerce GraphQl SDK based on operations
+ * and configuration defined in this module
+ */
+const sdkFactory = (config: BigCommerceModuleConfig) =>
+    getSdk(
+        requesterFactory({
+            graphqlEndpoint: config.graphqlEndpoint,
+            timeout: {
+                seconds: 10_000,
+                message: 'BigCommerce GraphQL request timed out',
+            },
+            onError: logAndThrowError,
+        })
+    );
+
+/**
+ * Types for the injectable BigCommerce GraphQl SDK
+ *
+ * Available calls, variables, and return types are derived from
+ * GraphQl operations defined in this module
+ */
+export type BcSdk = ReturnType<typeof sdkFactory>;
+
+/**
+ * Injection token for the BigCommerce GraphQl SDK
+ *
+ * Available calls, variables, and return types are derived from
+ * GraphQl operations defined in this module
+ *
+ * @example
+ * import { BigCommerceSdk } from '../../providers';
+ *
+ * const bcSdk = context.injector.get(BigCommerceSdk);
+ * response = await bcSdk.getBrands(variables, headers);
+ */
+export const BigCommerceSdk = new InjectionToken<BcSdk>(
+    'Sdk for making GraphQL calls to BigCommerce'
+);
+
+/**
+ * Utility type to assist with getting a deeply nested type by path
+ *
+ * Limited path autocompletion is provided and the return type is
+ * always non-nullable
+ *
+ * @example
+ * type BcBrandNode = GetProperty<GetBrandsQuery, 'site.brands.edges.0.node'>
+ * =>
+ * // type BcBrandNode = {
+ * //  __typename?: "Brand";
+ * //  id: string;
+ * //  ...
+ * // }
+ */
+export type GetProperty<T, Path extends Paths<T>> = NonNullable<Get<T, `${Path}`>>;
+
+/**
+ * Dependency injection setup for GraphQl Modules
+ * See documentation for more details:
+ * https://the-guild.dev/graphql/modules/docs/di/providers
+ */
 export const getProviders = (config: BigCommerceModuleConfig): Array<Provider> => {
     return [
         {
@@ -20,20 +81,17 @@ export const getProviders = (config: BigCommerceModuleConfig): Array<Provider> =
         },
         {
             provide: BigCommerceSdk,
-            useFactory: (config: BigCommerceModuleConfig) => {
-                const client = axios.create({
-                    baseURL: config.graphqlEndpoint,
-                    headers: {
-                        accept: 'application/json',
-                    },
-                    timeout: 10000,
-                    timeoutErrorMessage: 'BigCommerce GraphQL request timed out',
-                    signal: AbortSignal.timeout(10000),
-                });
-
-                const requester = getAxiosRequester(client, logAndThrowError);
-                return getSdk(requester);
-            },
+            useFactory: (config: BigCommerceModuleConfig) =>
+                getSdk(
+                    requesterFactory({
+                        graphqlEndpoint: config.graphqlEndpoint,
+                        timeout: {
+                            seconds: 10_000,
+                            message: 'BigCommerce GraphQL request timed out',
+                        },
+                        onError: logAndThrowError,
+                    })
+                ),
             deps: [ModuleConfig],
             global: true,
         },
