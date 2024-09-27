adab9557d1da8b36c837c9e117d4611e1efc8533
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2024-09-27 14:28:19 +0930

    MI-69: Mark isNotNull and removeFalseyValues as deprecated

--- packages/utils/src/is-not-null.ts
+++ packages/utils/src/is-not-null.ts
@@ -1,1 +1,6 @@
+/**
+ * @deprecated Use isTruthy from @aligent/utils instead. This will be removed in a future version.
+ * @param input
+ * @returns
+ */
 export const isNotNull = <T>(input: T | null): input is T => Boolean(input);
