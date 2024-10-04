fdfa01680ec0a7ac882e5adea0dc517457577879
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2024-10-04 14:21:57 +0930

    MI-73: Rename login file and add JSDocs

---
+++ packages/modules/auth/src/services/login-service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from 'graphql-modules';
+import { CustomerId } from '../types';
+
+/**
+ * Service for logging a user in to the primary backend platform
+ *
+ * This is a placeholder class and should be overridden by the project to provide
+ * the actual implementation.
+ *
+ * @example
+ * ```typescript
+ *  class ClientLoginService extends LoginService {
+ *      constructor(private sdk: ClientSdk) {
+ *          super();
+ *      }
+ *
+ *      override async login(args: LoginCredentials) {
+ *          return await this.sdk.login(...)
+ *      }
+ *  }
+ *
+ *  createModule({
+ *      ...
+ *      providers: [
+ *          {
+ *              provide: LoginService,
+ *              useClass: ClientLoginService
+ *          }
+ *      ]
+ *  })
+ * ```
+ */
+@Injectable({
+    global: true,
+})
+export class LoginService {
+    async login(args: unknown): Promise<CustomerId>;
+    async login(): Promise<CustomerId> {
+        throw new Error('Login service has not been implemented');
+    }
+}
