8de2302853101c583c8b34d0d3b2d749b4018071
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2024-10-24 12:40:41 +1030

    MI-3: Extract sdk builder from class constructor, use it to improve typescript inference
    This will now enforce that request options match AxiosRequestConfig shape - otherwise it's quite likely people will pass in headers that have no effect which would be annoying to debug

--- packages/modules/bigcommerce/src/clients/big-commerce-graphql-client.ts
+++ packages/modules/bigcommerce/src/clients/big-commerce-graphql-client.ts
@@ -16,7 +16,7 @@ import { logAndThrowError } from '@aligent/utils';
  * it's the only way I could find to get Typescript to understand the shape of the class with the SDK methods
  * dynamically added to it.
  */
-export interface BigCommerceGraphQlClient extends ReturnType<typeof getSdk> {}
+export interface BigCommerceGraphQlClient extends ReturnType<typeof buildBigCommerceAxiosSdk> {}
 
 /**
  * Class for making BigCommerce GraphQL requests
@@ -47,69 +47,77 @@ export class BigCommerceGraphQlClient {
     @ExecutionContext() private context: ExecutionContext;
 
     constructor(@Inject(forwardRef(() => ModuleConfig)) private config: BigCommerceModuleConfig) {
-        const timeout = {
-            milliseconds: 10_000,
-            message: 'BigCommerce GraphQL request timed out',
-        };
-        // The connection timeout should be slightly longer to
-        // allow servers to respond with a timeout failure before
-        // we directly cancel a connection
-        const connectionTimeout = timeout.milliseconds + 50;
-
-        const client = axios.create({
-            baseURL: config.graphqlEndpoint,
-            headers: {
-                accept: 'application/json',
-            },
-            timeout: timeout.milliseconds,
-            timeoutErrorMessage: timeout.message,
-        });
-
-        const requester = async <R, V>(
-            doc: DocumentNode,
-            variables: V,
-            options?: AxiosRequestConfig
-        ): Promise<R> => {
-            const operationName =
-                doc.definitions.find((d) => d.kind === Kind.OPERATION_DEFINITION)?.name?.value ||
-                'Unknown Operation';
-
-            try {
-                const customerImpersonationToken =
-                    await retrieveCustomerImpersonationTokenFromCache(this.context);
-
-                const data = {
-                    query: print(doc),
-                    variables,
-                };
-
-                const requestOptions = {
-                    signal: AbortSignal.timeout(connectionTimeout),
-                    ...options,
-                    headers: {
-                        Authorization: `Bearer ${customerImpersonationToken}`,
-                        ...options?.headers,
-                    },
-                };
-
-                const response = await xray.captureAsyncFunc(
-                    'BigCommerceGraphQl',
-                    async (segment) => {
-                        // Add query annotation to axios request
-                        segment?.addAnnotation('query', data.query);
-                        const response = await client.post<R>('', data, requestOptions);
-                        segment?.close();
-                        return response;
-                    }
-                );
-
-                return response.data;
-            } catch (error: unknown) {
-                return logAndThrowError(error, operationName);
-            }
-        };
-
-        const sdk = getSdk(requester);
+        const sdk = buildBigCommerceAxiosSdk(config, this.context);
         Object.assign(this, sdk);
     }
+}
+
+/**
+ * This function encapsulates the details of constructing an axios-based
+ * SDK for use by the class exported in this file.
+ *
+ * @returns Object with fully typed request functions generated from
+ * graphql operations defined in this module
+ */
+function buildBigCommerceAxiosSdk(config: BigCommerceModuleConfig, context: ExecutionContext) {
+    const timeout = {
+        milliseconds: 10_000,
+        message: 'BigCommerce GraphQL request timed out',
+    };
+    // The connection timeout should be slightly longer to
+    // allow servers to respond with a timeout failure before
+    // we directly cancel a connection
+    const connectionTimeout = timeout.milliseconds + 50;
+
+    const client = axios.create({
+        baseURL: config.graphqlEndpoint,
+        headers: {
+            accept: 'application/json',
+        },
+        timeout: timeout.milliseconds,
+        timeoutErrorMessage: timeout.message,
+    });
+
+    const requester = async <R, V>(
+        doc: DocumentNode,
+        variables: V,
+        options?: AxiosRequestConfig
+    ): Promise<R> => {
+        const operationName =
+            doc.definitions.find((d) => d.kind === Kind.OPERATION_DEFINITION)?.name?.value ||
+            'Unknown Operation';
+
+        try {
+            const customerImpersonationToken =
+                await retrieveCustomerImpersonationTokenFromCache(context);
+
+            const data = {
+                query: print(doc),
+                variables,
+            };
+
+            const requestOptions = {
+                signal: AbortSignal.timeout(connectionTimeout),
+                ...options,
+                headers: {
+                    Authorization: `Bearer ${customerImpersonationToken}`,
+                    ...options?.headers,
+                },
+            };
+
+            const response = await xray.captureAsyncFunc('BigCommerceGraphQl', async (segment) => {
+                // Add query annotation to axios request
+                segment?.addAnnotation('query', data.query);
+                const response = await client.post<R>('', data, requestOptions);
+                segment?.close();
+                return response;
+            });
+
+            return response.data;
+        } catch (error: unknown) {
+            return logAndThrowError(error, operationName);
+        }
+    };
+
+    return getSdk(requester);
 }
