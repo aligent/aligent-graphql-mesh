f3c3288d7b477ed6c1832e28361efc895af8cac2
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2024-10-30 00:30:44 +1030

    MI-19: Use customer service for clearCustomerCartResolver

--- packages/modules/bigcommerce/src/resolvers/mutations/clear-customer-cart.ts
+++ packages/modules/bigcommerce/src/resolvers/mutations/clear-customer-cart.ts
@@ -1,8 +1,7 @@
 import { MutationResolvers } from '@aligent/bigcommerce-resolvers';
 import { logAndThrowError } from '@aligent/utils';
-import { getBcCustomerId } from '../../utils';
-import { deleteCart } from '../../apis/graphql/delete-cart';
-import { retrieveCustomerImpersonationTokenFromCache } from '../../apis/rest';
+import { BigCommerceCustomerService } from '../../services';
+import { BigCommerceGraphQlClient } from '../../clients';
 
 export const UNDEFINED_CART = {
     id: '',
@@ -20,16 +19,23 @@ export const clearCustomerCartResolver = {
         if (!args.cartUid) {
             return logAndThrowError('Missing cart uid');
         }
-        const customerImpersonationToken =
-            await retrieveCustomerImpersonationTokenFromCache(context);
 
-        const bcCustomerId = getBcCustomerId(context);
+        const customerService = context.injector.get(BigCommerceCustomerService);
+        const headers = customerService.customerHeaders;
 
-        const response = await deleteCart(args.cartUid, customerImpersonationToken, bcCustomerId);
+        const graphQlClient = context.injector.get(BigCommerceGraphQlClient);
+        const status = await graphQlClient
+            .deleteCart({ cartEntityId: args.cartUid }, { headers })
+            .then((response) => {
+                if (response.errors) {
+                    logAndThrowError(response.errors);
+                }
+                return response.data?.cart.deleteCart;
+            });
 
         return {
             cart: UNDEFINED_CART,
-            status: !!response,
+            status: !!status,
         };
     },
 } satisfies MutationResolvers['clearCustomerCart'];
